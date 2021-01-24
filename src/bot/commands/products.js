80bc1b786b9024c115ac347c05ad599b81109e8d
MidSpike <developer@midspike.com>
2021-01-24 18:29:58 -0500

    products

--- src/bot/commands/products.js
+++ src/bot/commands/products.js
@@ -9,27 +9,23 @@ const { Discord, client } = require('../discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-await mongo();
-
-const db_roblox_products = (await productSchema.find({})).map(({
-    name,
-    code,
-    description,
- }) => ({
-    name,
-    code,
-    description,
-}));
-
-console.log({ db_roblox_products });
-
-//---------------------------------------------------------------------------------------------------------------//
-
 module.exports = {
     name: 'products',
     description: 'lists all of the products',
     aliases: ['products'],
     async execute(message, args, client) {
+        await mongo();
 
+        const db_roblox_products = (await productSchema.find({})).map(({
+            name,
+            code,
+            description,
+         }) => ({
+            name,
+            code,
+            description,
+        }));
+        
+        console.log({ db_roblox_products });
     },
 };
