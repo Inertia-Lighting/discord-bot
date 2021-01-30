1e873130ac010a45b31c9f50178b91894bf0587e
MidSpike <developer@midspike.com>
2021-01-30 17:23:02 -0500

    new command part 8

--- src/bot/commands/support.js
+++ src/bot/commands/support.js
@@ -47,6 +47,8 @@ module.exports = {
                 support_categories.map(({ index, name, description }) => `**${index + 1} | ${name}**\n${description}`).join('\n\n'),
                 '**Type the number of the category that you need.**',
             ].join('\n\n'),
-        }));
+        }), {
+            reply: message.author,
+        });
     },
 };
