fa872bd7387c4fbd1f2b2a79454315a058173eeb
MidSpike <developer@midspike.com>
2021-02-04 21:20:37 -0500

    fix_db p2

--- src/bot/commands/fix_db.js
+++ src/bot/commands/fix_db.js
@@ -15,6 +15,26 @@ module.exports = {
         const all_users = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, {});
         for (const user of all_users) {
             console.log({ user });
+
+            const new_user = { ...user };
+
+            if (new_user['_id']) new_user['discord_user_id'] = new_user['_id'];
+            if (new_user['ROBLOX_ID']) new_user['roblox_user_id'] = new_user['ROBLOX_ID'];
+
+            delete new_user['_id'];
+            delete new_user['ROBLOX_ID'];
+
+            await go_mongo_db.add(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, [
+                new_user,
+            ]);
+
+            await go_mongo_db.remove(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, {
+                '_id': user['_id'],
+            });
+
+            console.log({ new_user });
+
+            console.log('----------------------------------------------------------------------------------------------------------------');
         }
     },
 };
