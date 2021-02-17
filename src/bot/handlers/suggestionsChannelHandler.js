029772fe2f6e2326a97822540bd149913e70394c
MidSpike <developer@midspike.com>
2021-02-17 18:56:15 -0500

    cleanup

--- src/bot/handlers/suggestionsChannelHandler.js
+++ src/bot/handlers/suggestionsChannelHandler.js
@@ -8,25 +8,8 @@ const { Discord, client } = require('../discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const suggestion_cooldown_tracker = new Discord.Collection();
-
-//---------------------------------------------------------------------------------------------------------------//
-
 async function suggestionsChannelHandler(message) {
     const suggestions_channel = message.channel;
-
-    /* suggestion cooldown */
-    const suggestion_cooldown_in_ms = 10_000; // 10 seconds
-    const suggestion_cooldown_tracker_for_user = suggestion_cooldown_tracker.get(message.author.id);
-    const last_suggestion_epoch_for_user = suggestion_cooldown_tracker_for_user?.last_suggestion_epoch ?? Date.now() - suggestion_cooldown_in_ms;
-    const current_suggestion_cooldown_epoch = Date.now();
-    suggestion_cooldown_tracker.set(message.author.id, { last_suggestion_epoch: current_suggestion_cooldown_epoch });
-    if (current_suggestion_cooldown_epoch - last_suggestion_epoch_for_user < suggestion_cooldown_in_ms) {
-        const bot_cooldown_message = await message.reply('Please don\'t spam suggestions!').catch(console.warn);
-        bot_cooldown_message.delete({ timeout: 2_500 }); // remove the bot's message
-        message.delete({ timeout: 500 }); // remove the user's message since they are spamming
-        return;
-    }
 
     /* suggestion text */
     const suggestion_text = string_ellipses(message.cleanContent, 1000);
