10eb225d603e24e6c32b41be4879abad196d6494
MidSpike <developer@midspike.com>
2021-02-28 19:37:55 -0500

    fixes

--- src/bot/commands/fix_db.js
+++ src/bot/commands/fix_db.js
@@ -35,16 +35,15 @@ module.exports = {
             if (new_user['roblox_user_id'] && !new_user['identity']['roblox_user_id']) new_user['identity']['roblox_user_id'] = new_user['roblox_user_id'];
 
             if (!new_user['products']) new_user['products'] = {};
-            if (new_user['products']['Laser_Fixture']) new_user['products']['LASER_FIXTURE'] = new_user['products']['Laser_Fixture'];
-            if (new_user['products']['Follow_Spotlight']) new_user['products']['FOLLOW_SPOTLIGHT'] = new_user['products']['Follow_Spotlight'];
-            if (new_user['products']['C_Lights']) new_user['products']['C_LIGHTS'] = new_user['products']['C_Lights'];
-            if (new_user['products']['LED_Bars']) new_user['products']['LED_BARS'] = new_user['products']['LED_Bars'];
-            if (new_user['products']['MagicPanels']) new_user['products']['MAGIC_PANELS'] = new_user['products']['MagicPanels'];
-            if (new_user['products']['House_Lights']) new_user['products']['HOUSE_LIGHTS'] = new_user['products']['House_Lights'];
-            if (new_user['products']['Pars']) new_user['products']['PARS'] = new_user['products']['Pars'];
-            if (new_user['products']['Blinders']) new_user['products']['BLINDERS'] = new_user['products']['Blinders'];
-            if (new_user['products']['Wash']) new_user['products']['WASHES'] = new_user['products']['Wash'];
-
+            new_user['products']['LASER_FIXTURE'] = new_user['products']['Laser_Fixture'] ?? false;
+            new_user['products']['FOLLOW_SPOTLIGHT'] = new_user['products']['Follow_Spotlight'] ?? false;
+            new_user['products']['C_LIGHTS'] = new_user['products']['C_Lights'] ?? false;
+            new_user['products']['LED_BARS'] = new_user['products']['LED_Bars'] ?? false;
+            new_user['products']['MAGIC_PANELS'] = new_user['products']['MagicPanels'] ?? false;
+            new_user['products']['HOUSE_LIGHTS'] = new_user['products']['House_Lights'] ?? false;
+            new_user['products']['PARS'] = new_user['products']['Pars'] ?? false;
+            new_user['products']['BLINDERS'] = new_user['products']['Blinders'] ?? false;
+            new_user['products']['WASHES'] = new_user['products']['Wash'] ?? false;
             new_user['products']['SOURCE_4'] = false;
             new_user['products']['VISUALS'] = false;
 
@@ -53,6 +52,16 @@ module.exports = {
             delete new_user['ROBLOX_ID'];
             delete new_user['discord_user_id'];
             delete new_user['roblox_user_id'];
+
+            delete new_user['products']['Laser_Fixture'];
+            delete new_user['products']['Follow_Spotlight'];
+            delete new_user['products']['C_Lights'];
+            delete new_user['products']['LED_Bars'];
+            delete new_user['products']['MagicPanels'];
+            delete new_user['products']['House_Lights'];
+            delete new_user['products']['Pars'];
+            delete new_user['products']['Blinders'];
+            delete new_user['products']['Wash'];
 
             /* add the modified document to the new database */
             await go_mongo_db.add(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, [
