0a518420cb3e92db685788f98c03c1d914a0d16e
MidSpike <developer@midspike.com>
2021-03-02 14:48:42 -0500

    added command

---
+++ src/bot/commands/profile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+//---------------------------------------------------------------------------------------------------------------//
+
+const axios = require('axios');
+
+const { go_mongo_db } = require('../../mongo/mongo.js');
+
+const { Discord, client } = require('../discord_client.js');
+
+//---------------------------------------------------------------------------------------------------------------//
+
+module.exports = {
+    name: 'profile',
+    description: 'displays your profile',
+    aliases: ['profile'],
+    permission_level: 'staff',
+    async execute(message, args) {
+        const { command_args } = args;
+
+        const member_lookup_query = message.mentions.members.first()?.id ?? command_args[0];
+        const member = message.guild.members.resolve(member_lookup_query) ?? message.member;
+
+        const [ db_user_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, {
+            'identity.discord_user_id': member.user.id,
+        }, {
+            projection: {
+                '_id': false,
+            },
+        });
+
+        if (db_user_data) {
+            const db_roblox_products = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_PRODUCTS_COLLECTION_NAME, {});
+
+            const user_products = db_roblox_products.filter(product => db_user_data.products.includes(product.code));
+
+            const { data: roblox_user } = await axios.get(`https://users.roblox.com/v1/users/${encodeURIComponent(db_user_data.identity.roblox_user_id)}`);
+
+            await message.channel.send(new Discord.MessageEmbed({
+                color: 0x60A0FF,
+                author: {
+                    iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                    name: 'Inertia Lighting | User Profile',
+                },
+                fields: [
+                    {
+                        name: 'Discord',
+                        value: `<@${db_user_data.identity.discord_user_id}>`,
+                    }, {
+                        name: 'Roblox',
+                        value: `[${roblox_user.displayName}](https://roblox.com/users/${roblox_user.id}/profile)`,
+                    }, {
+                        name: 'Products',
+                        value: `${user_products.length === 0 ? 'n/a' : user_products.map(product => `- ${product.name}`).join('\n')}`,
+                    },
+                ],
+            })).catch(console.warn);
+        } else {
+            message.reply('You aren\'t in our database!');
+        }
+    },
+};
