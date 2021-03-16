6ceafff54211fc5cca2708e511e3e6ab46428e1f
MidSpike <developer@midspike.com>
2021-03-16 13:06:09 -0400

    support system improvements

--- src/bot/commands/support.js
+++ src/bot/commands/support.js
@@ -192,21 +192,23 @@ module.exports = {
                     await support_channel.send(`${message.author}, welcome to your support ticket!`).catch(console.warn);
 
                     /* send the user document */
-                    // const [ db_user_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, {
-                    //     'identity.discord_user_id': message.author.id,
-                    // }, {
-                    //     projection: {
-                    //         '_id': false,
-                    //     },
-                    // });
-                    // await support_channel.send(new Discord.MessageEmbed({
-                    //     color: 0x60A0FF,
-                    //     author: {
-                    //         iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                    //         name: 'Inertia Lighting | User Document',
-                    //     },
-                    //     description: `${'```'}json\n${JSON.stringify(db_user_data ?? 'user not found in database', null, 2)}\n${'```'}`,
-                    // })).catch(console.warn);
+                    /** @TODO Change database and collection for update Catalyst */
+                    const [ db_user_data ] = await go_mongo_db.find(process.env.MONGO_OLD_DATABASE_NAME, process.env.MONGO_OLD_USERS_COLLECTION_NAME, {
+                        'identity.discord_user_id': message.author.id,
+                    }, {
+                        projection: {
+                            /** @TODO uncomment for update Catalyst */
+                            // '_id': false,
+                        },
+                    });
+                    await support_channel.send(new Discord.MessageEmbed({
+                        color: 0x60A0FF,
+                        author: {
+                            iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                            name: 'Inertia Lighting | User Document',
+                        },
+                        description: `${'```'}json\n${JSON.stringify(db_user_data ?? 'user not found in database', null, 2)}\n${'```'}`,
+                    })).catch(console.warn);
 
                     /* send the blacklisted user document */
                     // const [ blacklisted_user_db_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_BLACKLISTED_USERS_COLLECTION_NAME, {
