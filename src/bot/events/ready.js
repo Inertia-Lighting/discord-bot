c95faaa2f8c573f4d183803ade04b4c80cfe3b56
MidSpike <developer@midspike.com>
2021-03-17 14:14:01 -0400

    added bot presences

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -10,6 +10,32 @@ const { client } = require('../discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
+const updateBotPresence = ((client) => {
+    const statuses = [
+        'with Roblox!',
+        'with !verify',
+        'with Inertia Lighting products!',
+    ];
+
+    return (async () => {
+        const first_status_item = statuses.shift(); // remove the first item and return it
+
+        const updated_presence = await client.user.setPresence({
+            status: 'online',
+            activity: {
+                type: 'PLAYING',
+                name: `${first_status_item}`,
+            },
+        });
+
+        statuses.push(first_status_item); // append first_status_item to the end of the array
+
+        return updated_presence;
+    });
+})(client);
+
+//---------------------------------------------------------------------------------------------------------------//
+
 module.exports = {
     name: 'ready',
     async handler() {
@@ -17,5 +43,8 @@ module.exports = {
         console.log(`----------------------------------------------------------------------------------------------------------------`);
         console.log(`Discord Bot Logged in as ${client.user.tag} on ${ready_timestamp}`);
         console.log(`----------------------------------------------------------------------------------------------------------------`);
+
+        /* update the bot presence every 5 minutes */
+        setInterval(async () => await updateBotPresence(), 5 * 60_000);
     },
 };
