f27b8f696c2d174c70bd0de0f326b0d5e73a816f
MidSpike <developer@midspike.com>
2021-03-29 16:07:26 -0400

    fixes: blacklist command

--- src/bot/commands/blacklist.js
+++ src/bot/commands/blacklist.js
@@ -12,11 +12,15 @@ const { Discord, client } = require('../discord_client.js');
 
 /**
  * Fetches a user in the users database
- * @param {String} discord_user_id
- * @param {String} roblox_user_id
+ * @param {Object} user_identity
+ * @param {String?} user_identity.discord_user_id
+ * @param {String?} user_identity.roblox_user_id
  * @returns {Promise<any|undefined>} db_user_data
  */
-async function findUserInUsersDatabase(discord_user_id=undefined, roblox_user_id=undefined) {
+async function findUserInUsersDatabase({ discord_user_id, roblox_user_id }) {
+    if (discord_user_id && typeof discord_user_id !== 'string') throw new TypeError('\`discord_user_id\` must be a string or undefined');
+    if (roblox_user_id && typeof roblox_user_id !== 'string') throw new TypeError('\`roblox_user_id\` must be a string or undefined');
+
     const [ db_user_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_USERS_COLLECTION_NAME, {
         ...(discord_user_id ? {
             'identity.discord_user_id': discord_user_id,
@@ -24,17 +28,55 @@ async function findUserInUsersDatabase(discord_user_id=undefined, roblox_user_id
             'identity.roblox_user_id': roblox_user_id,
         }),
     });
+
     return db_user_data;
 }
 
 /**
- * Adds a user to the blacklist
- * @param {*} db_user_data
- * @param {*} blacklist_metadata
- * @returns {Promise<boolean>} success or failure
+ * Fetches a user in the blacklisted-users database
+ * @param {Object} user_identity
+ * @param {String?} user_identity.discord_user_id
+ * @param {String?} user_identity.roblox_user_id
+ * @returns {Promise<any>} db_blacklisted_user_data
  */
-async function addUserToBlacklistedUsersDatabase({ identity: { discord_user_id, roblox_user_id } }, { epoch, reason, staff_member_id }) {
-    if (discord_user_id && roblox_user_id) {
+ async function findUserInBlacklistedUsersDatabase({ discord_user_id, roblox_user_id }) {
+    if (discord_user_id && typeof discord_user_id !== 'string') throw new TypeError('\`discord_user_id\` must be a string or undefined');
+    if (roblox_user_id && typeof roblox_user_id !== 'string') throw new TypeError('\`roblox_user_id\` must be a string or undefined');
+
+    const [ db_blacklisted_user_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_BLACKLISTED_USERS_COLLECTION_NAME, {
+        ...(discord_user_id ? {
+            'identity.discord_user_id': discord_user_id,
+        } : {
+            'identity.roblox_user_id': roblox_user_id,
+        }),
+    }, {
+        projection: {
+            '_id': false,
+        },
+    });
+
+    return db_blacklisted_user_data;
+}
+
+/**
+ * Adds a user to the blacklisted-users database
+ * @param {Object} user_identity
+ * @param {String} user_identity.discord_user_id
+ * @param {String} user_identity.roblox_user_id
+ * @param {Object} blacklist_metadata
+ * @param {Number} blacklist_metadata.epoch
+ * @param {String} blacklist_metadata.reason
+ * @param {String} blacklist_metadata.staff_member_id
+ * @returns {Promise<Boolean>} success or failure
+ */
+async function addUserToBlacklistedUsersDatabase({ discord_user_id, roblox_user_id }, { epoch, reason, staff_member_id }) {
+    if (typeof discord_user_id !== 'string') throw new TypeError('\`discord_user_id\` must be a string');
+    if (typeof roblox_user_id !== 'string') throw new TypeError('\`roblox_user_id\` must be a string');
+    if (typeof epoch !== 'number') throw new TypeError('\`epoch\` must be a number');
+    if (typeof reason !== 'string') throw new TypeError('\`reason\` must be a string');
+    if (typeof staff_member_id !== 'string') throw new TypeError('\`staff_member_id\` must be a string');
+
+    try {
         await go_mongo_db.update(process.env.MONGO_DATABASE_NAME, process.env.MONGO_BLACKLISTED_USERS_COLLECTION_NAME, {
             'identity.discord_user_id': discord_user_id,
             'identity.roblox_user_id': roblox_user_id,
@@ -57,19 +99,26 @@ async function addUserToBlacklistedUsersDatabase({ identity: { discord_user_id, 
         }, {
             upsert: true,
         });
-        return true; // user was added to blacklist
-    } else {
+    } catch (error) {
+        console.trace(error);
         return false; // user was not added to blacklist
     }
+
+    return true; // user was added to blacklist
 }
 
 /**
- * Removes a user from the blacklist
- * @param {*} db_user_data
- * @returns {Promise<boolean>} success or failure
+ * Removes a user from the blacklisted-users database
+ * @param {Object} user_identity
+ * @param {String} user_identity.discord_user_id
+ * @param {String} user_identity.roblox_user_id
+ * @returns {Promise<Boolean>} success or failure
  */
-async function removeUserFromBlacklistedUsersDatabase({ identity: { discord_user_id, roblox_user_id } }) {
-    if (discord_user_id && roblox_user_id) {
+async function removeUserFromBlacklistedUsersDatabase({ discord_user_id, roblox_user_id }) {
+    if (typeof discord_user_id !== 'string') throw new TypeError('\`discord_user_id\` must be a string');
+    if (typeof roblox_user_id !== 'string') throw new TypeError('\`roblox_user_id\` must be a string');
+
+    try {
         await go_mongo_db.remove(process.env.MONGO_DATABASE_NAME, process.env.MONGO_BLACKLISTED_USERS_COLLECTION_NAME, {
             'identity.discord_user_id': discord_user_id,
             'identity.roblox_user_id': roblox_user_id,
@@ -84,31 +133,12 @@ async function removeUserFromBlacklistedUsersDatabase({ identity: { discord_user
         }, {
             upsert: true,
         });
-        return true; // user was removed from blacklist
-    } else {
+    } catch (error) {
+        console.trace(error);
         return false; // user was not removed from blacklist
     }
-}
 
-/**
- * Looks up a user in the blacklist
- * @param {String} discord_user_id
- * @param {String} roblox_user_id
- * @returns {Promise<any>} blacklisted_user_db_data
- */
-async function lookupUserInBlacklistedUsersDatabase(discord_user_id=undefined, roblox_user_id=undefined) {
-    const [ blacklisted_user_db_data ] = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_BLACKLISTED_USERS_COLLECTION_NAME, {
-        ...(discord_user_id ? {
-            'identity.discord_user_id': discord_user_id,
-        } : {
-            'identity.roblox_user_id': roblox_user_id,
-        }),
-    }, {
-        projection: {
-            '_id': false,
-        },
-    });
-    return blacklisted_user_db_data;
+    return true; // user was removed from blacklist
 }
 
 //---------------------------------------------------------------------------------------------------------------//
@@ -117,22 +147,22 @@ async function lookupUserInBlacklistedUsersDatabase(discord_user_id=undefined, r
  * Check if the staff member is allowed to blacklist the potential user.
  * @param {Guild} guild
  * @param {String} staff_member_id
- * @param {String?} user_id
+ * @param {String} user_id
+ * @returns {Promise<Boolean>} 
  */
-async function checkIfStaffMemberIsAllowedToBlacklistUser(guild, staff_member_id, user_id) {
-    if (!staff_member_id) throw new Error('\`staff_member_id\` was undefined!');
-
-    if (!user_id) return false; // in the event that a user was not found in the users database, then they can't be blacklisted
+async function isStaffMemberAllowedToBlacklistUser(guild, staff_member_id, user_id) {
+    if (!staff_member_id) throw new TypeError('\`staff_member_id\` was undefined');
+    if (!user_id) throw new TypeError('\`user_id\` was undefined');
 
     /* don't let staff blacklist themselves */
     if (staff_member_id === user_id) return false;
 
     /* check that the staff member exists in the guild */
-    const staff_member = await guild.members.fetch(staff_member_id);
+    const staff_member = await guild.members.fetch(staff_member_id).catch(() => undefined);
     if (!staff_member) return false;
 
     /* check if the member exists in the guild */
-    const member_being_blacklisted = await guild.members.fetch(user_id);
+    const member_being_blacklisted = await guild.members.fetch(user_id).catch(() => undefined);
     if (!member_being_blacklisted) return true; // no need to check role hierarchy if the user isn't in the guild
 
     /* check the role hierarchy since they exist in the guild */
@@ -155,22 +185,25 @@ module.exports = {
         const lookup_discord_user_id = message.mentions.members.first()?.id;
         const lookup_roblox_user_id = command_args[1];
 
-        const db_user_data = await findUserInUsersDatabase(lookup_discord_user_id, lookup_roblox_user_id);
-
         switch (command_args[0]?.toLowerCase()) {
             case 'add':
+                const db_user_data = await findUserInUsersDatabase({
+                    discord_user_id: lookup_discord_user_id,
+                    roblox_user_id: lookup_roblox_user_id,
+                });
+
                 if (!db_user_data) {
                     message.reply('You can only blacklist users that have verified in the product hub!');
                     return;
                 }
 
-                const staff_member_can_add_user_to_blacklist = await checkIfStaffMemberIsAllowedToBlacklistUser(message.guild, staff_member_id, db_user_data?.identity?.discord_user_id);
+                const staff_member_can_add_user_to_blacklist = await isStaffMemberAllowedToBlacklistUser(message.guild, staff_member_id, db_user_data.identity.discord_user_id);
                 if (!staff_member_can_add_user_to_blacklist) {
                     message.reply('You aren\'t allowed to blacklist that user!');
                     return;
                 }
 
-                const added_successfully = await addUserToBlacklistedUsersDatabase(db_user_data, {
+                const added_successfully = await addUserToBlacklistedUsersDatabase(db_user_data.identity, {
                     epoch: Date.now(),
                     reason: command_args.slice(2).join(' ').trim() || 'no reason was specified',
                     staff_member_id: staff_member_id,
@@ -183,18 +216,23 @@ module.exports = {
 
                 break;
             case 'remove':
+                const db_user_data = await findUserInUsersDatabase({
+                    discord_user_id: lookup_discord_user_id,
+                    roblox_user_id: lookup_roblox_user_id,
+                });
+
                 if (!db_user_data) {
                     message.reply('You can only un-blacklist users that have verified in the product hub!');
                     return;
                 }
 
-                const staff_member_can_remove_user_from_blacklist = await checkIfStaffMemberIsAllowedToBlacklistUser(message.guild, staff_member_id, db_user_data?.identity?.discord_user_id);
+                const staff_member_can_remove_user_from_blacklist = await isStaffMemberAllowedToBlacklistUser(message.guild, staff_member_id, db_user_data.identity.discord_user_id);
                 if (!staff_member_can_remove_user_from_blacklist) {
                     message.reply('You aren\'t allowed to un-blacklist that user!');
                     return;
                 }
 
-                const removed_successfully = await removeUserFromBlacklistedUsersDatabase(db_user_data);
+                const removed_successfully = await removeUserFromBlacklistedUsersDatabase(db_user_data.identity);
                 if (removed_successfully) {
                     message.reply('I removed that user from the blacklist!');
                 } else {
@@ -203,21 +241,26 @@ module.exports = {
 
                 break;
             case 'lookup':
-                const blacklisted_user_db_data = await lookupUserInBlacklistedUsersDatabase(lookup_discord_user_id, lookup_roblox_user_id);
+                const db_blacklisted_user_data = await findUserInBlacklistedUsersDatabase({
+                    discord_user_id: lookup_discord_user_id,
+                    roblox_user_id: lookup_roblox_user_id,
+                });
+
                 message.channel.send(new Discord.MessageEmbed({
                     color: 0x60A0FF,
                     author: {
                         iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
                         name: 'Inertia Lighting | Blacklisted User Document',
                     },
-                    description: (blacklisted_user_db_data ? [
-                        `**User:** <@${blacklisted_user_db_data.identity.discord_user_id}>`,
-                        `**Roblox Id:** \`${blacklisted_user_db_data.identity.roblox_user_id}\``,
-                        `**Staff:** <@${blacklisted_user_db_data.staff_member_id}>`,
-                        `**Date:** \`${moment(blacklisted_user_db_data.epoch).tz('America/New_York').format('YYYY[-]MM[-]DD | hh:mm A | [GMT]ZZ')}\``,
-                        `**Reason:** ${'```'}\n${blacklisted_user_db_data.reason}\n${'```'}`,
+                    description: (db_blacklisted_user_data ? [
+                        `**User:** <@${db_blacklisted_user_data.identity.discord_user_id}>`,
+                        `**Roblox Id:** \`${db_blacklisted_user_data.identity.roblox_user_id}\``,
+                        `**Staff:** <@${db_blacklisted_user_data.staff_member_id}>`,
+                        `**Date:** \`${moment(db_blacklisted_user_data.epoch).tz('America/New_York').format('YYYY[-]MM[-]DD | hh:mm A | [GMT]ZZ')}\``,
+                        `**Reason:** ${'```'}\n${db_blacklisted_user_data.reason}\n${'```'}`,
                     ].join('\n') : `${'```'}\nUser is not blacklisted!\n${'```'}`),
                 })).catch(console.warn);
+
                 break;
             default:
                 message.reply(new Discord.MessageEmbed({
