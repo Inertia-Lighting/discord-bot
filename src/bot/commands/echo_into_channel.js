1a4e2036c1c0ae8613a64ab6245c20598d6dc3ed
Tyler Resch <developer@midspike.com>
2021-04-30 20:57:51 -0400

    General fixes and cleanup to recent commits

---
+++ src/bot/commands/echo_into_channel.js
@@ -0,0 +1,38 @@
+'use strict';
+
+//---------------------------------------------------------------------------------------------------------------//
+
+const { Discord, client } = require('../discord_client.js');
+
+//---------------------------------------------------------------------------------------------------------------//
+
+module.exports = {
+    name: 'echo_into_channel',
+    description: 'echoes what a user says into a specific channel',
+    aliases: ['echo_into_channel'],
+    permission_level: 'admin',
+    async execute(message, args) {
+        const { command_prefix, command_name, command_args } = args;
+
+        const [ potential_channel_id, command_args_to_echo... ] = command_args;
+        
+        const channel = client.channels.resolve(potential_channel_id);
+        if (!channel) {
+            message.reply([
+                'Please specify a valid channel!',
+                'Example:',
+                '\`\`\`',
+                '${command_prefix}${command_name} <#601972296185282571> Hello world!',
+                '\`\`\`',
+            ].join('\n')).catch(console.warn);
+            return;
+        }
+
+        const message_to_echo = command_args_to_echo.join(' ');
+
+        channel.send(`${message_to_echo}`).catch(console.warn);
+
+        await Timer(500); // prevent api abuse
+        message.delete().catch(console.warn);
+    },
+};
