b43bd3d8bd66ff28b17730a6516ee2bf21f71034
drawn <69976177+itsdrawn@users.noreply.github.com>
2021-04-30 20:27:21 -0400

    create echo_buff.js
    in progress

---
+++ src/bot/commands/echo_buff.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//---------------------------------------------------------------------------------------------------------------//
+
+const { Discord, client } = require('../discord_client.js');
+
+//---------------------------------------------------------------------------------------------------------------//
+
+module.exports = {
+    name: 'echo',
+    description: 'Buffed version of the previous command, echo.js. Contains a channel mention argument & content argument.',
+    aliases: ['echo'],
+    permission_level: 'admin',
+    async execute(message, args) {
+        const { command_args } = args;
+
+        message.delete({ timeout: 500 }).catch(console.warn);
+        message.channel.send(`${command_args.join(' ')}`).catch(console.warn);
+    },
+};
