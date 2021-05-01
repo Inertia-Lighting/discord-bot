0350ca10f0aa2ae4f99c582fd4c14bb743232a77
MidSpike <developer@midspike.com>
2021-05-01 17:42:36 -0400

    bug fix and added echo command

--- src/bot/commands/echo.js
+++ src/bot/commands/echo.js
@@ -5,27 +5,21 @@ const { Timer } = require('../../utilities.js');
 //---------------------------------------------------------------------------------------------------------------//
 
 module.exports = {
-    name: 'echo_into_channel',
-    description: 'echoes what a user says into a specific channel',
-    aliases: ['echo_into_channel'],
+    name: 'echo',
+    description: 'echoes what a user says into the same channel',
+    aliases: ['echo'],
     permission_level: 'admin',
     async execute(message, args) {
         const { command_prefix, command_name, command_args } = args;
 
-        const [ potential_channel_id='', ...command_args_to_echo ] = command_args;
+        const message_to_echo = command_args.join(' ').trim();
 
-        const parsed_potential_channel_id = potential_channel_id.replace(/[^0-9]/g, '');
-
-        const channel = message.guild.channels.resolve(parsed_potential_channel_id);
-
-        const message_to_echo = command_args_to_echo.join(' ').trim();
-
-        if (!channel || message_to_echo.length === 0) {
+        if (message_to_echo.length === 0) {
             message.reply([
-                'Please specify a valid channel and provide a message to echo!',
+                'Please provide a message to echo!',
                 'Example:',
                 '\`\`\`',
-                `${command_prefix}${command_name} <#601972296185282571> Hello world!`,
+                `${command_prefix}${command_name} Hello world!`,
                 '\`\`\`',
             ].join('\n')).catch(console.warn);
             return;
