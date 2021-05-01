885629d8483c7946db25e5a78a2bdafd707bc3b2
drawn <69976177+itsdrawn@users.noreply.github.com>
2021-04-30 20:32:27 -0400

    Update echo_buff.js

--- src/bot/commands/echo_buff.js
+++ src/bot/commands/echo_buff.js
@@ -14,7 +14,6 @@ module.exports = {
     async execute(message, args) {
         const { command_args } = args;
         console.log(args)
-        if(args === {}) return console.log("must provide content");
         message.delete({ timeout: 500 }).catch(console.warn);
         message.channel.send(`${command_args.join(' ')}`).catch(console.warn);
     },
