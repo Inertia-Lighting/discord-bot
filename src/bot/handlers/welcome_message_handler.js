2b6572ad4db0cd3b21a2ebe0f0056d10ea0b8402
Tyler Resch <developer@midspike.com>
2021-06-15 18:08:04 -0400

    fixes and cleanup

--- src/bot/handlers/welcome_message_handler.js
+++ src/bot/handlers/welcome_message_handler.js
@@ -10,11 +10,11 @@ const { Discord } = require('../discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const general_chat_id = '<#601890659439476766>';
-const support_channel_id = '<#814197612491833354>';
-const rules_channel_id = '<#601890376667889684>';
-const info_channel_id = '<#737156807910359091>';
-const news_channel_id = '<#854442081899642950>';
+const general_channel_id = '601890659439476766';
+const support_channel_id = '814197612491833354';
+const rules_channel_id = '601890376667889684';
+const info_channel_id = '737156807910359091';
+const news_channel_id = '854442081899642950';
 
 //---------------------------------------------------------------------------------------------------------------//
 
@@ -30,11 +30,11 @@ async function welcomeMessageHandler(member) {
             description: [
                 'Welcome to the Inertia Lighting discord server!',
                 'To get started, visit the following channels to learn about our server:',
-                `- ${general_chat_id}`, // general channel id
-                `- ${support_channel_id}`, // support channel id
-                `- ${rules_channel_id}`, // rules channel id
-                `- ${info_channel_id}`, // info channel id
-                `- ${news_channel_id}`, // news channel id
+                `- <#${rules_channel_id}>`,
+                `- <#${info_channel_id}>`,
+                `- <#${news_channel_id}>`,
+                `- <#${support_channel_id}>`,
+                `- <#${general_channel_id}>`,
                 'Also, check out our [product hub](https://www.roblox.com/games/5438256564) to purchase our products!',
             ].join('\n'),
         }),
@@ -44,7 +44,7 @@ async function welcomeMessageHandler(member) {
         const dm_channel = await member.createDM();
         await dm_channel.send(message_contents);
     } catch {
-        const general_chat_channel = client.channels.resolve(general_chat_id);
+        const general_chat_channel = client.channels.resolve(general_channel_id);
         await general_chat_channel.send(message_contents).catch(console.warn);
     }
 }
