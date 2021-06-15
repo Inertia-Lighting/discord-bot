3378a9df37b2a3f1364c86faf16a47dc0385fc26
drawn <69976177+itsdrawn@users.noreply.github.com>
2021-06-15 16:25:16 -0400

    added embed & messagehandling

--- src/bot/handlers/welcome_message_handler.js
+++ src/bot/handlers/welcome_message_handler.js
@@ -1,13 +1,47 @@
-//             message.channel.send(new Discord.MessageEmbed({
-//                 color: 0xFF0000,
-//                 author: {
-//                     iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-//                     name: `${client.user.username}`,
-//                 },
-//                 title: 'Error',
-//                 description: [
-//                     'That verification code was not recognized!',
-//                     `You need to provide the verification code that was given to you in the [Product Hub](${process.env.ROBLOX_PRODUCT_HUB_URL})!`,
-//                     `Example: \`${command_prefix}verify CODE_HERE\``,
-//                 ].join('\n'),
-//             })).catch(console.warn);
+/* Copyright © Inertia Lighting | All Rights Reserved */
+
+//---------------------------------------------------------------------------------------------------------------//
+
+'use strict';
+
+//---------------------------------------------------------------------------------------------------------------//
+
+const { Discord } = require('../discord_client.js');
+
+//---------------------------------------------------------------------------------------------------------------//
+
+const message_contents = {
+    color: 0,
+    author: {
+        iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+        name: `${client.user.username}`,
+    },
+    title: 'Welcome!',
+    description: [
+        'Welcome to Inertia Lighting, the largest lighting company at scale on ROBLOX sourcing some of today\'s best gear.',
+        'To get yourself started, view the following channels to obtain an understanding of our server.',
+        'Here for our product hub? View [**this!**](https://www.roblox.com/games/5438256564)',
+        'Require Support? View <#814197612491833354>**!**',
+        'Just looking to interact with the community? Go to <#601890659439476766>**!**',
+    ].join('\n'),
+}
+
+const general_chat_id = 601890659439476766
+
+//---------------------------------------------------------------------------------------------------------------//
+
+async function welcomeMessageHandler(member) {
+  try {
+        const dm_channel = await member.createDM();
+        dm_channel.send(member, new Discord.MessageEmbed(message_contents)).catch(console.warn);
+  } catch { /*user has their dms off, send in general*/
+        const general_chat_channel = client.channels.resolve(general_chat_id);
+        await general_chat_channel.send(`${member} due to your privacy settings not allowing me to direct message you, this embed was sent here!`, new Discord.MessageEmbed(message_contents)).catch(console.warn);
+  } 
+}
+
+//---------------------------------------------------------------------------------------------------------------//
+
+module.exports = {
+    welcomeMessageHandler,
+};
