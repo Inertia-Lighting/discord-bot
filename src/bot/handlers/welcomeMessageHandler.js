b494ef218a8da01b1afe3cdc7a9f9dd5a15205da
drawn <69976177+itsdrawn@users.noreply.github.com>
2021-06-15 15:51:15 -0400

    Create welcomeMessageHandler.js

---
+++ src/bot/handlers/welcomeMessageHandler.js
@@ -0,0 +1,19 @@
+/* Copyright © Inertia Lighting | All Rights Reserved */
+
+//---------------------------------------------------------------------------------------------------------------//
+
+'use strict';
+
+//---------------------------------------------------------------------------------------------------------------//
+
+async function welcomeMessageHandler(member) {
+    try {
+        await /*sending dm here*/
+    } catch {} // ignore any errors, as they will most likely be related to missing permissions
+}
+
+//---------------------------------------------------------------------------------------------------------------//
+
+module.exports = {
+    welcomeMessageHandler,
+};
