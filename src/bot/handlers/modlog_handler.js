878d24dc4c10fa9a73d5a8c523fb4b9d1c40deb4
Tyler Resch <developer@midspike.com>
2021-06-20 12:34:31 -0400

    Loads of fixes

--- src/bot/handlers/modlog_handler.js
+++ src/bot/handlers/modlog_handler.js
@@ -11,27 +11,41 @@ const { go_mongo_db } = require('../../mongo/mongo.js');
 //---------------------------------------------------------------------------------------------------------------//
 
 /**
- * Adds moderation log to database
- * @param {Object} identity
- * @param {String} identity.discord_user_id - The user id
- * @param {Object} record
- * @param {String} record.type - The moderation action type
- * @param {Number} record.epoch - The epoch of when the moderation action happened
- * @param {String} record.staff_member_id - The staff user id
- * @param {String} record.reason - The reason of the moderation action
- * @returns {Promise<Boolean>} success or failure
+ * @typedef {'WARN'|'MUTE'|'KICK'|'BAN'} ModerationActionType
  */
 
-async function logModerationActionToDatabase({ discord_user_id }, { type, epoch, staff_member_id, reason }) {
-    go_mongo_db.add(process.env.MONGO_DATABASE_NAME, process.env.MONGO_MODLOG_USER_RECORDS_COLLECTION_NAME, [
-        {
-            discord_user_id,
-            type,
-            epoch,
-            staff_member_id,
-            reason,
-        },
-    ]);
+//---------------------------------------------------------------------------------------------------------------//
+
+/**
+ * Adds moderation log to database
+ * @param {Object} identity
+ * @param {String} identity.discord_user_id
+ * @param {Object} record
+ * @param {ModerationActionType} record.type
+ * @param {Number} record.epoch
+ * @param {String} record.reason
+ * @param {String} record.staff_member_id
+ * @returns {Promise<Boolean>} success or failure
+ */
+async function logModerationActionToDatabase({ discord_user_id }, { type, epoch, reason, staff_member_id }) {
+    try {
+        await go_mongo_db.update(process.env.MONGO_DATABASE_NAME, process.env.MONGO_MODERATION_ACTION_RECORDS_COLLECTION_NAME, {
+            'identity.discord_user_id': discord_user_id,
+        }, {
+            $set: {
+                'record.type': type,
+                'record.epoch': epoch,
+                'record.reason': reason,
+                'record.staff_member_id': staff_member_id,
+            },
+        }, {
+            upsert: true,
+        });
+    } catch {
+        return false;
+    }
+
+    return true;
 }
 
 module.exports = {
