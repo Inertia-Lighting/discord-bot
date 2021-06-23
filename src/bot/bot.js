c483e5bc19128d0491609a7e3dbda90d65ccb0bb
MidSpike <developer@midspike.com>
2021-06-23 17:15:20 -0400

    fixes

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -15,17 +15,6 @@ const { client } = require('./discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const command_files_path = path.join(process.cwd(), './src/bot/commands/');
-const command_files = fs.readdirSync(command_files_path).filter(file => file.endsWith('.js'));
-
-/* register commands */
-for (const command_file of command_files) {
-    const bot_command = require(path.join(command_files_path, command_file));
-    client.$.commands.set(bot_command.name, bot_command);
-}
-
-//---------------------------------------------------------------------------------------------------------------//
-
 const event_files_path = path.join(process.cwd(), './src/bot/events/');
 const event_files = fs.readdirSync(event_files_path).filter(file => file.endsWith('.js'));
 
