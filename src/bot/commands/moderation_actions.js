89559571d59d31d928c324df6759468b2b20a42b
MidSpike <developer@midspike.com>
2021-06-26 23:49:05 -0400

    changes

--- src/bot/commands/moderation_actions.js
+++ src/bot/commands/moderation_actions.js
@@ -41,6 +41,117 @@ const { Discord, client } = require('../discord_client.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
+async function listModerationActionsForMember(message, { sub_command_name, sub_command_args }) {
+    /* lookup query for database */
+    const db_member_id_lookup_query = message.mentions.members?.first()?.id ?? undefined;
+
+    /* send an initial message to the user */
+    const bot_message = await message.channel.send(new Discord.MessageEmbed({
+        color: 0x60A0FF,
+        description: 'Loading moderation actions...',
+    }));
+
+    /* create a small user-experience delay */
+    await Timer(500);
+
+    /* fetch all moderation actions from the database */
+    const db_moderation_actions = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_MODERATION_ACTION_RECORDS_COLLECTION_NAME, {
+        'identity.discord_user_id': db_member_id_lookup_query,
+    });
+
+    /* check if the member has any records */
+    if (db_moderation_actions.length === 0) {
+        await bot_message.edit(new Discord.MessageEmbed({
+            color: 0xFFFF00,
+            author: {
+                iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                name: 'Inertia Lighting | Moderation Actions',
+            },
+            description: 'I wasn\'t able to find any moderation actions for the specified member!',
+        })).catch(console.warn);
+        return;
+    }
+
+    /* sort the moderation actions by epoch (newest -> oldest) */
+    const sorted_moderation_actions = db_moderation_actions.sort((a, b) => b.record.epoch - a.record.epoch);
+
+    /* split the moderation actions into a 2-dimensional array of chunks */
+    const moderation_actions_chunks = array_chunks(sorted_moderation_actions, 10);
+
+    /* send a carousel containing 10 moderation actions per page */
+    let page_index = 0;
+
+    async function editEmbedWithNextModerationActionsChunk() {
+        const moderation_actions_chunk = moderation_actions_chunks[page_index];
+
+        await bot_message.edit(new Discord.MessageEmbed({
+            color: 0x60A0FF,
+            author: {
+                iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                name: 'Inertia Lighting | Moderation Actions',
+            },
+            description: moderation_actions_chunk.map(moderation_action =>
+                [
+                    `**Staff** <@${moderation_action.record.staff_member_id}>`,
+                    `**Member** <@${moderation_action.identity.discord_user_id}>`,
+                    `**Date** \`${moment(moderation_action.record.epoch).tz('America/New_York').format('YYYY[-]MM[-]DD | hh:mm A | [GMT]ZZ')}\``,
+                    `**Type** \`${moderation_action.record.type}\``,
+                    '**Reason**',
+                    '\`\`\`',
+                    `${string_ellipses(moderation_action.record.reason, 250)}`,
+                    '\`\`\`',
+                ].join('\n')
+            ).join('\n'),
+        })).catch(console.warn);
+
+        return; // complete async
+    }
+
+    await editEmbedWithNextModerationActionsChunk();
+    await bot_message.react('⬅️');
+    await bot_message.react('➡️');
+    await bot_message.react('⏹️');
+
+    const message_reaction_filter = (collected_reaction, user) => user.id === message.author.id;
+    const message_reaction_collector = bot_message.createReactionCollector(message_reaction_filter, {
+        time: 5 * 60_000, // 5 minutes
+    });
+
+    message_reaction_collector.on('collect', async (collected_reaction) => {
+        message_reaction_collector.resetTimer();
+
+        switch (collected_reaction.emoji.name) {
+            case '⬅️': {
+                page_index = page_index < moderation_actions_chunks.length - 1 ? page_index + 1 : 0;
+                break;
+            }
+            case '➡️': {
+                page_index = page_index > 0 ? page_index - 1 : moderation_actions_chunks.length - 1;
+                break;
+            }
+            case '⏹️': {
+                message_reaction_collector.stop();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        if (message_reaction_collector.ended) return;
+
+        await editEmbedWithNextModerationActionsChunk();
+        await Timer(250);
+        await purgeUserReactionsFromMessage(bot_message);
+    });
+
+    message_reaction_collector.on('end', async () => {
+        await bot_message.delete();
+    });
+}
+
+//---------------------------------------------------------------------------------------------------------------//
+
 module.exports = {
     name: 'moderation_actions',
     description: 'displays moderation actions from the database',
@@ -48,93 +159,33 @@ module.exports = {
     permission_level: 'staff',
     cooldown: 10_000,
     async execute(message, args) {
-        /* send an initial message to the user */
-        const bot_message = await message.channel.send(new Discord.MessageEmbed({
-            color: 0x60A0FF,
-            description: 'Loading moderation actions...',
-        }));
+        const { command_prefix, command_name, command_args } = args;
 
-        /* create a small user-experience delay */
-        await Timer(500);
+        const sub_command_name = `${command_args[0]}`.toLowerCase();
+        const sub_command_args = command_args.slice(1);
 
-        /* fetch all moderation actions from the database */
-        const db_moderation_actions = await go_mongo_db.find(process.env.MONGO_DATABASE_NAME, process.env.MONGO_MODERATION_ACTION_RECORDS_COLLECTION_NAME, {});
+        switch (sub_command_name) {
+            case 'for': {
+                await listModerationActionsForMember(message, { ...args, sub_command_name, sub_command_args });
 
-        /* sort the moderation actions by epoch (newest -> oldest) */
-        const sorted_moderation_actions = db_moderation_actions.sort((a, b) => b.record.epoch - a.record.epoch);
-
-        /* split the moderation actions into a 2-dimensional array of chunks */
-        const moderation_actions_chunks = array_chunks(sorted_moderation_actions, 10);
-
-        /* send a carousel containing 10 moderation actions per page */
-        let page_index = 0;
-
-        async function editEmbedWithNextModerationActionsChunk() {
-            const moderation_actions_chunk = moderation_actions_chunks[page_index];
-
-            await bot_message.edit(new Discord.MessageEmbed({
-                color: 0x60A0FF,
-                author: {
-                    iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                    name: 'Inertia Lighting | Moderation Actions',
-                },
-                description: moderation_actions_chunk.map(moderation_action =>
-                    [
-                        `**Staff** <@${moderation_action.record.staff_member_id}>`,
-                        `**Member** <@${moderation_action.identity.discord_user_id}>`,
-                        `**Date** \`${moment(moderation_action.record.epoch).tz('America/New_York').format('YYYY[-]MM[-]DD | hh:mm A | [GMT]ZZ')}>\``,
-                        `**Type** \`${moderation_action.record.type}\``,
-                        '**Reason**',
-                        '\`\`\`',
-                        `${string_ellipses(moderation_action.record.reason, 250)}`,
-                        '\`\`\`',
-                    ].join('\n')
-                ).join('\n'),
-            })).catch(console.warn);
-
-            return; // complete async
-        }
-
-        await editEmbedWithNextModerationActionsChunk();
-        await bot_message.react('⬅️');
-        await bot_message.react('➡️');
-        await bot_message.react('⏹️');
-
-        const message_reaction_filter = (collected_reaction, user) => true;
-        const message_reaction_collector = bot_message.createReactionCollector(message_reaction_filter, {
-            time: 5 * 60_000, // 5 minutes
-        });
-
-        message_reaction_collector.on('collect', async (collected_reaction) => {
-            message_reaction_collector.resetTimer();
-
-            switch (collected_reaction.emoji.name) {
-                case '⬅️': {
-                    page_index = page_index < moderation_actions_chunks.length - 1 ? page_index + 1 : 0;
-                    break;
-                }
-                case '➡️': {
-                    page_index = page_index > 0 ? page_index - 1 : moderation_actions_chunks.length - 1;
-                    break;
-                }
-                case '⏹️': {
-                    message_reaction_collector.stop();
-                    break;
-                }
-                default: {
-                    break;
-                }
+                break;
             }
+            default: {
+                await message.channel.send(new Discord.MessageEmbed({
+                    color: 0x60A0FF,
+                    author: {
+                        iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                        name: 'Inertia Lighting | Moderation Actions',
+                    },
+                    title: 'Here are the available sub-commands!',
+                    description: [
+                        `- ${command_prefix}${command_name} for USER_HERE`,
+                        `- ${command_prefix}${command_name} clear USER_HERE`,
+                    ].join('\n'),
+                })).catch(console.warn);
 
-            if (message_reaction_collector.ended) return;
-
-            await editEmbedWithNextModerationActionsChunk();
-            await Timer(250);
-            await purgeUserReactionsFromMessage(bot_message);
-        });
-
-        message_reaction_collector.on('end', async () => {
-            await bot_message.delete();
-        });
+                break;
+            }
+        }
     },
 };
