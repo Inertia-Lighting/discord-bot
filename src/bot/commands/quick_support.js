994d0c155220ca6bfa508528f5f13efba07975b2
DJHARDY2K21 <tyyorkie05@gmail.com>
2021-07-13 04:35:34 +0100

    Fixed minor bug

--- src/bot/commands/quick_support.js
+++ src/bot/commands/quick_support.js
@@ -142,7 +142,7 @@ const qs_topics = [
             'download',
             'download products'
         ],
-        support_contents: 'To download your files, please visit the (website)[https://inertia.lighting/products]!'
+        support_contents: 'To download your files, please visit the [website](https://inertia.lighting/products)!'
     }
 ];
 
