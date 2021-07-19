3023e0c34e735f0443e52311e10ac61c01b1bb77
MidSpike <developer@midspike.com>
2021-07-19 10:12:52 -0400

    updated identity tokens

--- src/bot/commands/identity_token.js
+++ src/bot/commands/identity_token.js
@@ -19,13 +19,14 @@ const { command_permission_levels } = require('../common/bot.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const identity_token_channel_id = process.env.BOT_IDENTITY_TOKEN_CHANNEL_ID;
-
-//---------------------------------------------------------------------------------------------------------------//
-
 async function generateUserAPIToken() {
     const non_encrypted_token = uuid_v4();
-    const encrypted_token = bcrypt.hashSync(non_encrypted_token, bcrypt.genSaltSync(parseInt(process.env.USER_API_TOKEN_BCRYPT_SALT_LENGTH)));
+    const user_api_token_bcrypt_salt_length = Number.parseInt(process.env.USER_API_TOKEN_BCRYPT_SALT_LENGTH);
+
+    if (Number.isNaN(user_api_token_bcrypt_salt_length)) throw new Error('Invalid \`USER_API_TOKEN_BCRYPT_SALT_LENGTH\` environment variable.');
+
+    const encrypted_token = bcrypt.hashSync(non_encrypted_token, bcrypt.genSaltSync(user_api_token_bcrypt_salt_length));
+
     return {
         non_encrypted_token,
         encrypted_token,
@@ -48,31 +49,35 @@ module.exports = {
         });
 
         if (!db_user_data) {
-            await message.reply(`Please \`${command_prefix}verify\` before using this command!`).catch(console.warn);
-            return; // don't continue if they don't exist in the database
+            await message.reply({
+                content: `Please \`${command_prefix}verify\` before using this command!`,
+            }).catch(console.warn);
+            return; // don't continue if user doesn't exist in the database
         }
 
         switch (`${command_args[0]}`.toLowerCase()) {
             case 'help': {
-                message.channel.send(new Discord.MessageEmbed({
-                    color: 0x60A0FF,
-                    author: {
-                        iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                        name: 'Inertia Lighting | Identity Token System',
-                    },
-                    fields: [
-                        {
-                            name: 'What are Identity Tokens?',
-                            value: 'Identity Tokens are like passwords, we use them to verify your identity with our systems; just like how your computer uses a password to verify that it is you!',
-                        }, {
-                            name: 'How do I generate my Identity Token?',
-                            value: `Run \`${command_prefix}${command_name} generate\` to generate your token!`,
-                        }, {
-                            name: 'How do I use my Identity Token?',
-                            value: `Check out <#${identity_token_channel_id}> for instructions on how to use identity tokens!`,
+                await message.channel.send({
+                    embed: new Discord.MessageEmbed({
+                        color: 0x60A0FF,
+                        author: {
+                            iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                            name: 'Inertia Lighting | Identity Token System',
                         },
-                    ],
-                })).catch(console.warn);
+                        fields: [
+                            {
+                                name: 'What are Identity Tokens?',
+                                value: 'Identity Tokens are like passwords, we use them to verify your identity with our systems; just like how your computer uses a password to verify that it is you!',
+                            }, {
+                                name: 'How do I generate my Identity Token?',
+                                value: `Run \`${command_prefix}${command_name} generate\` to generate your token!`,
+                            }, {
+                                name: 'How do I use my Identity Token?',
+                                value: `When running the \`${command_prefix}${command_name} generate\` command, you will be given an Identity Token Code Snippet to paste into your Roblox Studio Command Bar.`,
+                            },
+                        ],
+                    }),
+                }).catch(console.warn);
 
                 break;
             }
@@ -81,24 +86,36 @@ module.exports = {
 
                 try {
                     const dm_channel = await message.author.createDM();
-                    await dm_channel.send(new Discord.MessageEmbed({
-                        color: 0x60A0FF,
-                        author: {
-                            iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                            name: 'Inertia Lighting | Identity Token System',
-                        },
-                        description: [
-                            'The following token must be kept private and must be used in games with our products:',
-                            `||${non_encrypted_token}||`,
-                            '(click the box above to view your token)',
-                        ].join('\n'),
-                    }));
+                    await dm_channel.send({
+                        embed: new Discord.MessageEmbed({
+                            color: 0x60A0FF,
+                            author: {
+                                iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
+                                name: 'Inertia Lighting | Identity Token System',
+                            },
+                            description: [
+                                '**Instructions:**',
+                                '1) Keep your Identity Token secret, it is unique to you (like a password).',
+                                '2) Paste your Identity Token Code Snippet into Roblox Studio\'s Command Bar.',
+                                '3) Observe your Roblox Studio\'s Output.',
+                                '4) Assuming everything went correctly, you can now use our products inside of your game.',
+                                '',
+                                '**Your Identity Token Code Snippet:**',
+                                `||\`require(7096701514)('${non_encrypted_token}')\`||`,
+                                '(Click the box above to view your Identity Token Code Snippet)',
+                            ].join('\n'),
+                        }),
+                    });
                 } catch {
-                    await message.reply('I was unable to DM you!').catch(console.warn);
-                    return; // don't continue if the user can't be messaged via DMs
+                    await message.reply({
+                        content: 'I was unable to send a direct message to you!',
+                    }).catch(console.warn);
+                    return; // don't continue if the user can't be direct messaged
                 }
 
-                await message.reply('I sent you your Identity Token via DMs!').catch(console.warn);
+                await message.reply({
+                    content: 'I sent you your Identity Token via DMs!',
+                }).catch(console.warn);
 
                 try {
                     await go_mongo_db.update(process.env.MONGO_DATABASE_NAME, process.env.MONGO_API_AUTH_USERS_COLLECTION_NAME, {
@@ -113,7 +130,9 @@ module.exports = {
                     });
                 } catch (error) {
                     console.trace(error);
-                    await message.reply('Something went wrong while updating your Identity Token in the database, please contact staff!').catch(console.warn);
+                    await message.reply({
+                        content: 'Something went wrong while updating your Identity Token in the database, please contact our support staff!',
+                    }).catch(console.warn);
                 }
 
                 break;
