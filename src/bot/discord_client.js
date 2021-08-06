c9e92019865d7c27934e0e6d5bc2967b6dca4fd0
MidSpike <developer@midspike.com>
2021-08-06 11:32:28 -0400

    fixes

--- src/bot/discord_client.js
+++ src/bot/discord_client.js
@@ -20,9 +20,11 @@ const client = new Discord.Client({
     },
     intents: [
         Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
         Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
         Discord.Intents.FLAGS.GUILD_WEBHOOKS,
         Discord.Intents.FLAGS.GUILD_INVITES,
         Discord.Intents.FLAGS.GUILD_BANS,
