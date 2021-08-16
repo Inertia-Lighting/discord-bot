cdd22061306a526b80f12fb1befc26e875d01cd3
MidSpike <developer@midspike.com>
2021-08-16 14:16:00 -0400

    Fixes sensitivity of Illegal Nickname handler

--- src/bot/handlers/illegal_nickname_handler.js
+++ src/bot/handlers/illegal_nickname_handler.js
@@ -10,20 +10,34 @@ const { Timer } = require('../../utilities.js');
 
 //---------------------------------------------------------------------------------------------------------------//
 
-const non_ascii_regex_filter = /[^A-Z0-9\-\_\'\#\(\)\[\]\{\}\|\s]/gi;
+const non_allowed_regex_filter = /[^\w\d]|_/gi;
 
 const display_name_override_nickname = 'Illegal Nickname';
-const display_name_override_reason = 'The user\'s display name contained non-ascii character(s).';
+const display_name_override_reason = 'The user\'s display name contained too many non-allowed characters.';
 
 //---------------------------------------------------------------------------------------------------------------//
 
+/**
+ * @typedef {import('discord.js').GuildMember} GuildMember
+ */
+
+//---------------------------------------------------------------------------------------------------------------//
+
+/**
+ * @param {GuildMember} member
+ */
 async function illegalNicknameHandler(member) {
+    if (member.user.bot) return;
+    if (member.user.system) return;
     if (!member.manageable) return; // the user has a higher role than the bot, so don't continue
 
     const user_display_name = `${member.displayName}`; // force to be a string
-    const non_ascii_occurrences = user_display_name.match(non_ascii_regex_filter) ?? []; // force to be an array
+    const non_allowed_occurrences = user_display_name.match(non_allowed_regex_filter) ?? []; // force to be an array
 
-    if (non_ascii_occurrences.length === 0) return; // only proceed if there are non-ascii characters
+    const non_allowed_occurrences_threshold = user_display_name.length / 4; // 1/5th of the display name
+    const non_allowed_occurrences_threshold_exceeded = non_allowed_occurrences.length > non_allowed_occurrences_threshold;
+
+    if (!non_allowed_occurrences_threshold_exceeded) return; // only proceed if the non-allowed character threshold is exceeded
 
     await Timer(5_000); // try to prevent the user from spamming nickname changes
 
