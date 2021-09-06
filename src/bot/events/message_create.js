a88645c98b906ccd595cf0eee0777739f2a311d3
William Fortson <williamfortson3@gmail.com>
2021-09-06 14:58:32 -0500

    made requested changes

--- src/bot/events/message_create.js
+++ src/bot/events/message_create.js
@@ -36,14 +36,6 @@ module.exports = {
             return;
         }
 
-        /* roles handler */
-        message.guild.roles.cache.forEach((role) => {
-            const role_name = role.name.toLowerCase();
-            if (role_name.includes('duolingo')) {
-                role.delete();
-            }
-        });
-
         /* respond to mentions of this bot */
         if (message.content.startsWith(`<@!${client.user.id}>`)) {
             message.reply({
