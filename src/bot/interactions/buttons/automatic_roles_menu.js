f4bf22768b4cce376de01c2524b0779c2b523912
MidSpike <developer@midspike.com>
2021-10-23 11:07:33 -0400

    updates

--- src/bot/interactions/buttons/automatic_roles_menu.js
+++ src/bot/interactions/buttons/automatic_roles_menu.js
@@ -12,22 +12,38 @@
 
 //---------------------------------------------------------------------------------------------------------------//
 
+const automatic_role_ids = [
+    '835003393734737952',
+    '835003401812574228',
+    '835011400086716426',
+    '835003400882094090',
+    '835003345348722758',
+    '891339307527335936',
+];
+
+//---------------------------------------------------------------------------------------------------------------//
+
 module.exports = {
     identifier: 'automatic_roles_menu',
     /** @param {SelectMenuInteraction} interaction */
     async execute(interaction) {
         if (!interaction.guildId) return;
 
-        const automatic_role_ids = interaction.values;
+        await interaction.deferUpdate();
 
-        for (const automatic_role_id of automatic_role_ids) {
-            const member = await interaction.guild.members.fetch(interaction.user.id);
+        const guild_member = await interaction.guild.members.fetch(interaction.userId);
 
-            if (member.roles.cache.has(automatic_role_id)) {
-                member.roles.remove(automatic_role_id);
-            } else {
-                member.roles.add(automatic_role_id);
-            }
+        const role_ids = interaction.values;
+
+        const roles_to_add = automatic_role_ids.filter(automatic_role_id => role_ids.includes(automatic_role_id));
+        const roles_to_remove = automatic_role_ids.filter(automatic_role_id => !role_ids.includes(automatic_role_id));
+
+        if (roles_to_add.length > 0) {
+            await guild_member.roles.add(roles_to_add);
+        }
+
+        if (roles_to_remove.length > 0) {
+            await guild_member.roles.remove(roles_to_remove);
         }
     },
 };
