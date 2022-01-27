3b1a52101e256e1caf55193284048ab3be13e039
MidSpike <developer@midspike.com>
2022-01-27 12:33:32 -0500

    mini updates

--- src/bot/handlers/automated_quick_support_handler.js
+++ src/bot/handlers/automated_quick_support_handler.js
@@ -40,7 +40,7 @@ async function updateQuickSupportTopics() {
     }
 }
 setImmediate(() => updateQuickSupportTopics());
-setInterval(() => updateQuickSupportTopics(), 10 * 60_000); // every 10 minutes
+setInterval(() => updateQuickSupportTopics(), 15 * 60_000); // every 15 minutes
 
 async function findPotentialMatchingQuickSupportTopics(user_input) {
     const mapped_quick_support_topics = [];
@@ -88,18 +88,18 @@ async function automatedQuickSupportHandler(message) {
 
     message.reply({
         content: [
-            `I found ${matching_qs_topics.length} quick support topics that might be relevant to your message!`,
+            `I found ${matching_qs_topics.length} quick support topics that might be related to your message!`,
         ].join('\n'),
         embeds: matching_qs_topics.map(quick_support_topic => ({
             color: 0x60A0FF,
             author: {
                 iconURL: `${client.user.displayAvatarURL({ dynamic: true })}`,
-                name: 'Inertia Lighting | Quick Support System',
+                name: 'Inertia Lighting | Automatic Quick Support',
             },
             title: quick_support_topic.title,
             description: quick_support_topic.support_contents,
             footer: {
-                text: `${quick_support_topic.similarity_score.toFixed(2)}%`,
+                text: `Automated Confidence: ${(Math.clamp(quick_support_topic.similarity_score, 0, 1) * 100).toFixed(2)}%`,
             },
         })),
     }).catch(console.warn);
