ac138f2d48efb3342b2630a0bf737d8496857b6c
MidSpike <developer@midspike.com>
2023-06-13 20:42:21 -0400

    updates: messages logger

--- src/bot/handlers/logs/guild_member_messages.ts
+++ src/bot/handlers/logs/guild_member_messages.ts
@@ -2,22 +2,23 @@
 //    Copyright (c) Inertia Lighting, Some Rights Reserved    //
 //------------------------------------------------------------//
 
-//---------------------------------------------------------------------------------------------------------------//
-
 import { Discord, client } from '../../discord_client';
 
 import { getMarkdownFriendlyTimestamp, string_ellipses } from '../../../utilities';
 
 import { CustomEmbed } from '@root/bot/common/message';
 
-//---------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------//
 
 const logging_channel_id = process.env.BOT_LOGGING_CHANNEL_ID as string;
 if (typeof logging_channel_id !== 'string') throw new TypeError('logging_channel_id is not a string');
 
-//---------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------//
 
-async function guildMemberMessageUpdateLogger(old_message: Discord.Message, new_message: Discord.Message) {
+export async function guildMemberMessageUpdateLogger(
+    old_message: Discord.Message,
+    new_message: Discord.Message,
+) {
     if (new_message.author.bot) return;
     if (new_message.author.system) return;
     if (old_message.content === new_message.content) return;
@@ -26,6 +27,14 @@ async function guildMemberMessageUpdateLogger(old_message: Discord.Message, new_
     if (!(logging_channel instanceof Discord.TextChannel)) throw new Error('Failed to fetch logging channel');
 
     const message_update_timestamp = getMarkdownFriendlyTimestamp(new_message.editedTimestamp ?? Date.now());
+
+    const old_message_attachments_list = old_message.attachments.map(
+        (attachment) => `\`${attachment.name}\` - [link](${attachment.url})`
+    );
+
+    const new_message_attachments_list = new_message.attachments.map(
+        (attachment) => `\`${attachment.name}\` - [link](${attachment.url})`
+    );
 
     await logging_channel.send({
         embeds: [
@@ -46,12 +55,20 @@ async function guildMemberMessageUpdateLogger(old_message: Discord.Message, new_
                         value: `<t:${message_update_timestamp}:F> (<t:${message_update_timestamp}:R>)`,
                         inline: false,
                     }, {
-                        name: 'Before',
+                        name: 'Content Before',
                         value: old_message.content.length > 0 ? string_ellipses(Discord.escapeMarkdown(old_message.content), 2048) : '\`n/a\`',
                         inline: false,
                     }, {
-                        name: 'After',
+                        name: 'Content After',
                         value: new_message.content.length > 0 ? string_ellipses(Discord.escapeMarkdown(new_message.content), 2048) : '\`n/a\`',
+                        inline: false,
+                    }, {
+                        name: 'Attachments Before',
+                        value: old_message_attachments_list.length > 0 ? old_message_attachments_list.join('\n') : '\`n/a\`',
+                        inline: false,
+                    }, {
+                        name: 'Attachments After',
+                        value: new_message_attachments_list.length > 0 ? new_message_attachments_list.join('\n') : '\`n/a\`',
                         inline: false,
                     },
                 ],
@@ -60,7 +77,9 @@ async function guildMemberMessageUpdateLogger(old_message: Discord.Message, new_
     }).catch(console.trace);
 }
 
-async function guildMemberMessageDeleteLogger(message: Discord.Message) {
+export async function guildMemberMessageDeleteLogger(
+    message: Discord.Message,
+) {
     if (message.author.bot) return;
     if (message.author.system) return;
 
@@ -68,6 +87,10 @@ async function guildMemberMessageDeleteLogger(message: Discord.Message) {
     if (!(logging_channel instanceof Discord.TextChannel)) throw new Error('Failed to fetch logging channel');
 
     const message_delete_timestamp = getMarkdownFriendlyTimestamp(Date.now());
+
+    const message_attachments_list = message.attachments.map(
+        (attachment) => `\`${attachment.name}\` - [link](${attachment.url})`
+    );
 
     await logging_channel.send({
         embeds: [
@@ -90,16 +113,13 @@ async function guildMemberMessageDeleteLogger(message: Discord.Message) {
                         name: 'Content',
                         value: message.content.length > 0 ? string_ellipses(Discord.escapeMarkdown(message.content), 2048) : '\`n/a\`',
                         inline: false,
+                    }, {
+                        name: 'Attachments',
+                        value: message_attachments_list.length > 0 ? message_attachments_list.join('\n') : '\`n/a\`',
+                        inline: false,
                     },
                 ],
             }),
         ],
     }).catch(console.trace);
 }
-
-//---------------------------------------------------------------------------------------------------------------//
-
-export {
-    guildMemberMessageUpdateLogger,
-    guildMemberMessageDeleteLogger,
-};
