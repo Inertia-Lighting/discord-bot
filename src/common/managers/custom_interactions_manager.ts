f4c658568bce578efd87ae231d542c44dbcd908d
MidSpike <developer@midspike.com>
2023-08-09 16:57:34 -0400

    never trust autocomplete

--- src/common/managers/custom_interactions_manager.ts
+++ src/common/managers/custom_interactions_manager.ts
@@ -278,7 +278,7 @@ export class CustomInteractionsManager {
             const access_levels_for_user = [ CustomInteractionAccessLevel.Public ]; // default access level
 
             const bot_guild = await discord_client.guilds.fetch(bot_guild_id);
-            const bot_guild_member = await bot_guild.members.fetch(discord_client.user);
+            const bot_guild_member = await bot_guild.members.fetch(interaction.user.id);
             const bot_guild_member_roles_cache = bot_guild_member.roles.cache;
 
             if (bot_guild_member_roles_cache.has(guild_staff_role_id)) {
