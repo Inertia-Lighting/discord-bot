1ad2c8a2a5e5ed72eba5565fc18d46d0ac3b1cc7
MidSpike <developer@midspike.com>
2023-08-12 22:16:54 -0400

    fix embed style

--- src/common/handlers/support_system_handler.ts
+++ src/common/handlers/support_system_handler.ts
@@ -218,14 +218,20 @@ export const support_categories: SupportCategory[] = [
                     }),
                 ],
             });
+
             await support_ticket_channel.send({
-                content: [
-                    `${Discord.userMention(support_ticket_owner.id)}, as an initial troubleshooting step, please follow the instructions below while you wait for a response from our support staff.`,
-                    '',
-                    '`StreamingEnabled` is not compatible with most of our products.',
-                    '',
-                    'Please follow the steps in this [guide](https://youtu.be/xApLkcuXwVk) to disable `game.Workspace.StreamingEnabled`.',
-                ].join('\n'),
+                content: `${Discord.userMention(support_ticket_owner.id)}`,
+                embeds: [
+                    CustomEmbed.from({
+                        description: [
+                            'Please follow the instructions below while waiting to hear back from a support staff member.',
+                            '',
+                            '\`game.Workspace.StreamingEnabled == true\` is not compatible with most of our products.',
+                            '',
+                            'Follow the steps in [this guide](https://youtu.be/xApLkcuXwVk) to disable \`game.Workspace.StreamingEnabled\`.',
+                        ].join('\n'),
+                    }),
+                ],
             });
         },
     }, {
