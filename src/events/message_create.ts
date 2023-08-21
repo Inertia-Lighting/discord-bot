a1173f647633657ba39506a823951f5ece64395f
Walter Robinson <chessebuilderman@gmail.com>
2023-08-20 18:01:01 -0700

    modified:   src/events/message_create.ts

--- src/events/message_create.ts
+++ src/events/message_create.ts
@@ -5,7 +5,6 @@
 import * as Discord from 'discord.js';
 
 import { automatedQuickSupportHandler, suggestionsCategoryHandler } from '@root/common/handlers';
-import { fetchHighestAccessLevelForUser } from '../common/permissions';
 
 //------------------------------------------------------------//
 
