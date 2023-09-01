00357526ed68264e6ebbac85c66e6dc3fbed9f42
chessebuilderman <41169564+chessebuilderman@users.noreply.github.com>
2023-09-01 14:34:22 -0700

    Create guides.ts

---
+++ src/custom_interactions/chat_input/guides.ts
@@ -0,0 +1,41 @@
+//------------------------------------------------------------//
+//    Copyright (c) Inertia Lighting, Some Rights Reserved    //
+//------------------------------------------------------------//
+
+import * as Discord from 'discord.js';
+
+import { CustomEmbed } from '@root/common/message';
+
+import { CustomInteraction, CustomInteractionAccessLevel, CustomInteractionRunContext, CustomInteractionsManager } from '@root/common/managers/custom_interactions_manager';
+
+//------------------------------------------------------------//
+
+export default new CustomInteraction({
+    identifier: 'help',
+    type: Discord.InteractionType.ApplicationCommand,
+    data: {
+        type: Discord.ApplicationCommandType.ChatInput,
+        description: 'Lists all available commands for you to use!',
+        options: [],
+    },
+    metadata: {
+        required_run_context: CustomInteractionRunContext.Guild,
+        required_access_level: CustomInteractionAccessLevel.Public,
+    },
+    handler: async (discord_client, interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.inCachedGuild()) return;
+        if (!interaction.channel) return;
+
+        await interaction.deferReply({ ephemeral: false });
+
+        await interaction.editReply({
+            embeds: [
+                CustomEmbed.from({
+                    title: 'Guides',
+                    description: 'https://inertia.lighting/guides',
+                }),
+            ],
+        });
+    },
+});
