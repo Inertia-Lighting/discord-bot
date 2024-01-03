23b417c3c443584d2e9033863306053700e12e7a
Walter Robinson <chessebuilderman@gmail.com>
2024-01-02 17:09:10 -0800

    Add logging for GUILD_BAN_ADD and MEMBER_KICK
    Signed-off-by: Walter Robinson <chessebuilderman@gmail.com>

--- src/common/handlers/logs/guild_member_retention.ts
+++ src/common/handlers/logs/guild_member_retention.ts
@@ -96,9 +96,112 @@ async function guildMemberRemoveLogger(
     }).catch(console.trace);
 }
 
+async function guildMemberBannedLogger(
+    member: Discord.GuildMember,
+) {
+    if (!(member instanceof Discord.GuildMember)) throw new TypeError('guildMemberBannedLogger(): member is not a GuildMember');
+
+    const client = member.guild.client;
+
+    const member_retention_logging_channel = await client.channels.fetch(member_retention_logging_channel_id);
+    if (!member_retention_logging_channel) throw new Error('Failed to fetch logging channel');
+    if (!member_retention_logging_channel.isTextBased()) throw new TypeError('member_retention_logging_channel is not a text channel');
+
+    const user_creation_timestamp = getMarkdownFriendlyTimestamp(member.user.createdTimestamp);
+    const member_joined_timestamp = getMarkdownFriendlyTimestamp(member.joinedTimestamp ?? Date.now());
+    const member_left_timestamp = getMarkdownFriendlyTimestamp(Date.now());
+
+    await member_retention_logging_channel.send({
+        embeds: [
+            CustomEmbed.from({
+                color: CustomEmbed.Color.Orange,
+                title: 'A member has been banned from the server!',
+                fields: [
+                    {
+                        name: 'Member',
+                        value: `@${member.user.username} (${member.user.id})`,
+                        inline: false,
+                    }, {
+                        name: 'Account creation date',
+                        value: `<t:${user_creation_timestamp}:F> (<t:${user_creation_timestamp}:R>)`,
+                    }, {
+                        name: 'Account join date',
+                        value: `<t:${member_joined_timestamp}:F> (<t:${member_joined_timestamp}:R>)`,
+                    }, {
+                        name: 'Account banned date',
+                        value: `<t:${member_left_timestamp}:F> (<t:${member_left_timestamp}:R>)`,
+                    }, {
+                        name: 'Account stayed for',
+                        value: `${Math.floor((Number.parseInt(member_left_timestamp, 10) - Number.parseInt(member_joined_timestamp, 10)) / (60 * 60 * 24))} days`,
+                    },
+                    {
+                        name: 'Account Banned for',
+                        value: `${(await member.guild.bans.fetch(`${member.user.id}`)).reason ?? 'No reason was added'}`,
+                    },
+                ],
+            }),
+        ],
+    }).catch(console.trace);
+}
+
+async function guildMemberKickedLogger(
+    member: Discord.GuildMember,
+) {
+    if (!(member instanceof Discord.GuildMember)) throw new TypeError('guildMemberKickedLogger(): member is not a GuildMember');
+
+    const client = member.guild.client;
+
+    const member_retention_logging_channel = await client.channels.fetch(member_retention_logging_channel_id);
+    if (!member_retention_logging_channel) throw new Error('Failed to fetch logging channel');
+    if (!member_retention_logging_channel.isTextBased()) throw new TypeError('member_retention_logging_channel is not a text channel');
+
+    const user_creation_timestamp = getMarkdownFriendlyTimestamp(member.user.createdTimestamp);
+    const member_joined_timestamp = getMarkdownFriendlyTimestamp(member.joinedTimestamp ?? Date.now());
+    const member_left_timestamp = getMarkdownFriendlyTimestamp(Date.now());
+
+    const audit_log = await member.guild.fetchAuditLogs({
+        limit: 1,
+        type: Discord.AuditLogEvent.MemberKick,
+    });
+    await member_retention_logging_channel.send({
+        embeds: [
+            CustomEmbed.from({
+                color: CustomEmbed.Color.Orange,
+                title: 'A member has been kicked from the server!',
+                fields: [
+                    {
+                        name: 'Member',
+                        value: `@${member.user.username} (${member.user.id})`,
+                        inline: false,
+                    }, {
+                        name: 'Account creation date',
+                        value: `<t:${user_creation_timestamp}:F> (<t:${user_creation_timestamp}:R>)`,
+                    }, {
+                        name: 'Account join date',
+                        value: `<t:${member_joined_timestamp}:F> (<t:${member_joined_timestamp}:R>)`,
+                    }, {
+                        name: 'Account banned date',
+                        value: `<t:${member_left_timestamp}:F> (<t:${member_left_timestamp}:R>)`,
+                    }, {
+                        name: 'Account stayed for',
+                        value: `${Math.floor((Number.parseInt(member_left_timestamp, 10) - Number.parseInt(member_joined_timestamp, 10)) / (60 * 60 * 24))} days`,
+                    },
+                    {
+                        name: 'Account Kicked for',
+                        value: `${audit_log.entries.first()?.reason ?? 'No reason was added'}`,
+                    },
+                ],
+            }),
+        ],
+    }).catch(console.trace);
+}
+
+
 //------------------------------------------------------------//
 
 export {
     guildMemberAddLogger,
     guildMemberRemoveLogger,
+    guildMemberBannedLogger,
+    guildMemberKickedLogger,
 };
---
+++ src/events/guild_ban_add.ts
@@ -0,0 +1,29 @@
+//------------------------------------------------------------//
+//    Copyright (c) Inertia Lighting, Some Rights Reserved    //
+//------------------------------------------------------------//
+
+import * as Discord from 'discord.js';
+
+import { guildMemberBannedLogger } from '@root/common/handlers';
+
+//------------------------------------------------------------//
+
+const bot_guild_id = `${process.env.BOT_GUILD_ID ?? ''}`;
+if (bot_guild_id.length < 1) throw new Error('environment variable: BOT_GUILD_ID; was not properly set or is empty');
+
+//------------------------------------------------------------//
+
+export default {
+    name: Discord.Events.GuildBanAdd,
+    async handler(
+        client: Discord.Client,
+        member: Discord.GuildMember,
+    ) {
+        if (member.user.system) return; // don't operate on system accounts
+        if (member.user.bot) return; // don't operate on bots to prevent feedback-loops
+        if (member.guild.id !== bot_guild_id) return; // don't operate on other guilds
+
+        /* log members leaving */
+        await guildMemberBannedLogger(member).catch(console.trace);
+    },
+};
