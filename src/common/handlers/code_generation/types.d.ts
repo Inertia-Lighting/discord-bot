025e12b4be2eabf1bb78c9737304010bc282493e
Walter Robinson <chessebuilderman@gmail.com>
2024-07-02 22:21:21 -0700

    Convert to JSON DB
    Convert code generation storage to a local JSON file.

---
+++ src/common/handlers/code_generation/types.d.ts
@@ -0,0 +1,14 @@
+import { UserUpdateEmitter } from './user_update';
+export interface verification_code_data {
+    interaction: CommandInteraction,
+    roblox_id: string,
+    code: string,
+    expiration: number
+    event: events.EventEmitter
+    message_object: Message;
+}
+
+export type user_db_type = {
+    codes: verification_code_data[];
+    events: Map<string, UserUpdateEmitter>
+}
