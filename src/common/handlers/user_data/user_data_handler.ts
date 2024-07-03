14827d775442f448ded5c73f26effcd3ebeb7583
Walter Robinson <chessebuilderman@gmail.com>
2024-07-03 14:55:13 -0700

    Remove API Reference from profile
    Removed API refrence from profile and directly call database instead

---
+++ src/common/handlers/user_data/user_data_handler.ts
@@ -0,0 +1,13 @@
+import { DbUserData, DbUserDataArray } from '@root/types';
+
+export async function dbUserArray(
+    db_user_data: DbUserData
+): Promise<DbUserDataArray> {
+    const product_array: string[] = Object.keys(db_user_data.products).filter(product => db_user_data.products[product])
+    return {
+        _id: db_user_data._id,
+    identity: db_user_data.identity,
+    lumens: db_user_data.lumens,
+    products: product_array
+    }
+}
