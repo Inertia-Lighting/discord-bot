6a870754d194b1f50831a391b8c823bd549cc3f1
Walter Robinson <chessebuilderman@gmail.com>
2025-03-13 17:40:14 -0700

    Remove old bot stuff

--- src/common/handlers/logs/index.ts
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------//
-//    Copyright (c) Inertia Lighting, Some Rights Reserved    //
-//------------------------------------------------------------//
-
-export * from './guild_member_messages';
-
-export * from './guild_member_retention';
-
-export * from './guild_member_roles';
