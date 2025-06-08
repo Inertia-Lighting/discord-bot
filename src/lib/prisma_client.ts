c389a2850dc13d5d4707cb699afe0359371c4bba
Walter Robinson <chessebuilderman@gmail.com>
2025-06-08 13:36:19 -0700

    Add prisma client

---
+++ src/lib/prisma_client.ts
@@ -0,0 +1,31 @@
+// import { createHmac } from 'node:crypto';
+
+import { PrismaClient } from './prisma';
+
+
+// import SnowflakeGenerator from './SnowflakeGenerator';v
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient({
+    log: ['error', 'info', 'warn'],
+    omit: {
+        user: {
+            createdAt: true,
+            updatedAt: true
+        },
+        products: {
+            id: true,
+            updatedAt: true
+        },
+        punishments: {
+            id: true,
+        },
+    }
+});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// const snowflakeGenerator = new SnowflakeGenerator(929);
+
+export default prisma;
