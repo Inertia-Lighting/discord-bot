8bb63fe1aea7516bfdc55721f2bead35999a7e20
Inertia Server <server@inertia.lighting>
2025-06-12 04:24:25 -0400

    Suggestion filter

--- src/common/handlers/suggestions_category_handler.ts
+++ src/common/handlers/suggestions_category_handler.ts
@@ -3,9 +3,9 @@
 // ------------------------------------------------------------//
 
 import { CustomEmbed } from '@root/common/message';
-import { delay, ellipseString } from '@root/utilities';
-import * as Discord from 'discord.js';
-import { findBestMatch } from 'string-similarity';
+import { delay, ellipseString } from '@root/utilities/index';
+import { findBestMatch, Rating } from 'string-similarity';
+import Discord from 'discord.js'
 
 // ------------------------------------------------------------//
 
@@ -20,10 +20,24 @@ const camera_words: string[] = [
     'cam',
     'live camera',
     'working camera',
+    'cody cam pack',
+    'cam pack',
+    'cody'
 ];
 
-function checkCameraMention(msg: string): boolean {
-    return (findBestMatch(msg, camera_words).ratings.some((rating) => rating.rating > 0.5));
+interface coolStuff {
+    match: boolean;
+    detected: Rating[];
+}
+
+function checkCameraMention(msg: string): coolStuff {
+    const check = findBestMatch(msg, camera_words)
+    const match = check.ratings.some((rating) => rating.rating > 0.4)
+    console.log(match)
+    return ({
+        match,
+        detected: check.ratings
+    });
 }
 
 export async function suggestionsCategoryHandler(
@@ -32,15 +46,16 @@ export async function suggestionsCategoryHandler(
     if (!message.member) return;
     if (message.author.system || message.author.bot) return;
     if (message.content.length === 0) return;
-
+    const isStaff = message.member.roles.cache.has(bot_staff_role_id)
     /* don't allow staff to create suggestions */
-    if (message.member.roles.cache.has(bot_staff_role_id) && !message.content.startsWith('!s ')) {
+    if (isStaff && !message.content.startsWith('!s ')) {
         return;
     }
 
     const suggestions_channel = message.channel;
-    if(!suggestions_channel.isSendable()) throw new Error('The identity manager logging channel is not sendable!');
-    if (checkCameraMention(message.content)) {
+    const check = checkCameraMention(message.content)
+    const highest = check.detected.sort((a, b) => a.rating - b.rating)
+    if (check.match) {
         const camera_message = await message.reply({
             embeds: [
                 CustomEmbed.from({
@@ -55,19 +70,33 @@ export async function suggestionsCategoryHandler(
 \n Resource Demands: More staff or contributors may be needed to manage and support the system.
 \n Functionality Constraints: There are strict limitations on what can and cannot be achieved, especially within platforms like Roblox.
 \n Additionally, Roblox cameras attempt to render everything within their view. This means that when a camera faces a densely populated or complex scene, it may attempt to load everything, potentially causing system slowdowns or complete failure to render.`,
+footer: {
+    text: `Detected: ${highest[0].target}, ${highest[0].rating}`
+}
                 }),
             ],
         });
+        let msg = ''
+        highest.forEach((rate) => {
+            msg = msg + ` ${rate.rating}/${rate.target}`
+        })
+        const ratingmsg = await message.channel.send({
+            content: msg
+        })
         message.delete();
         setTimeout(() => {
             camera_message.delete();
-        }, 5_000);
+            ratingmsg.delete()
+        }, 30_000);
         return;
     }
 
     /* suggestion text */
-    let suggestion_text = message.content.slice(3);
-    suggestion_text = ellipseString(message.content, 1000);
+    let suggestion_text: string = message.content
+    if(isStaff) suggestion_text = suggestion_text.slice(3);
+    console.log(suggestion_text)
+    suggestion_text = ellipseString(suggestion_text, 1000);
+    console.log(suggestion_text)
 
     /* suggestion embed */
     const bot_suggestion_message = await suggestions_channel.send({
