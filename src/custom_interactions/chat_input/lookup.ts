ae325af367397347a868cffa6d3839d711f020e4
Walter Robinson <me@walterr.cc>
2025-07-13 14:21:57 -0700

    Fix blacklist issue

--- src/custom_interactions/chat_input/lookup.ts
+++ src/custom_interactions/chat_input/lookup.ts
@@ -23,200 +23,196 @@ if (db_blacklisted_users_collection_name.length < 1) throw new Error('Environmen
 // ------------------------------------------------------------//
 
 export default new CustomInteraction({
-    identifier: 'lookup',
-    type: Discord.InteractionType.ApplicationCommand,
-    data: {
-        type: Discord.ApplicationCommandType.ChatInput,
-        description: 'Used by staff to lookup a user\'s database record.',
+  identifier: 'lookup',
+  type: Discord.InteractionType.ApplicationCommand,
+  data: {
+    type: Discord.ApplicationCommandType.ChatInput,
+    description: 'Used by staff to lookup a user\'s database record.',
+    options: [
+      {
+        name: 'discord',
+        type: Discord.ApplicationCommandOptionType.Subcommand,
+        description: 'Uses discord ids to lookup a user.',
         options: [
-            {
-                name: 'discord',
-                type: Discord.ApplicationCommandOptionType.Subcommand,
-                description: 'Uses discord ids to lookup a user.',
-                options: [
-                    {
-                        name: 'user',
-                        type: Discord.ApplicationCommandOptionType.User,
-                        description: 'The discord user to lookup.',
-                        required: true,
-                    },
-                    {
-                        name: 'properties',
-                        type: Discord.ApplicationCommandOptionType.String,
-                        description: 'Properties of the user to look up',
-                        choices: [
-                            { name: 'General', value: 'general' },
-                            { name: 'Transactions', value: 'transactions' },
-                            { name: 'Transfers', value: 'transfers' },
-                            { name: 'Punishments', value: 'punishments' }
-                        ],
-                        required: false,
-                    },
-                    {
-                        name: 'ephemeral',
-                        type: Discord.ApplicationCommandOptionType.Boolean,
-                        description: 'Whether or not to respond with an ephemeral message.',
-                        required: false,
-                    },
-                ],
-            }, {
-                name: 'roblox',
-                type: Discord.ApplicationCommandOptionType.Subcommand,
-                description: 'Uses roblox ids to lookup a user.',
-                options: [
-                    {
-                        name: 'user',
-                        type: Discord.ApplicationCommandOptionType.String,
-                        description: 'The roblox user id to lookup.',
-                        required: true,
-                    },
-                    {
-                        name: 'properties',
-                        type: Discord.ApplicationCommandOptionType.String,
-                        description: 'Properties of the user to look up',
-                        choices: [
-                            { name: 'General', value: 'general' },
-                            { name: 'Transactions', value: 'transactions' },
-                            { name: 'Transfers', value: 'transfers' },
-                            { name: 'Punishments', value: 'punishments' }
-                        ],
-                        required: false,
-                    },
-                    {
-                        name: 'ephemeral',
-                        type: Discord.ApplicationCommandOptionType.Boolean,
-                        description: 'Whether or not to respond with an ephemeral message.',
-                        required: false,
-                    },
-                ],
-            },
-
-        ],
-    },
-    metadata: {
-        required_run_context: CustomInteractionRunContext.Guild,
-        required_access_level: CustomInteractionAccessLevel.Staff,
-    },
-    handler: async (discord_client, interaction) => {
-        if (!interaction.isChatInputCommand()) return;
-        if (!interaction.inCachedGuild()) return;
-        if (!interaction.channel) return;
-
-        const subcommand_name = interaction.options.getSubcommand(true);
-        const selection = interaction.options.getString('properties', false) as 'general' | 'transactions' | 'transfers' | 'punishments'
-        const respond_as_ephemeral: boolean = interaction.options.getBoolean('ephemeral', false) ?? false; // default to false
-
-        await interaction.deferReply({ flags: respond_as_ephemeral ? ['Ephemeral'] : [] });
-
-        let user_id: string;
-        switch (subcommand_name) {
-            case 'discord': {
-                user_id = interaction.options.getUser('user', true).id;
-
-                break;
-            }
-
-            case 'roblox': {
-                user_id = interaction.options.getString('user', true);
-
-                break;
-            }
-
-            default: {
-                throw new Error(`Unknown subcommand name: ${subcommand_name}`);
-            }
-        }
-
-        const user_id_regex = /^\d+$/;
-        if (!user_id_regex.test(user_id)) {
-            await interaction.editReply({
-                content: 'You supplied an invalid user id.',
-            });
-
-            return;
-        }
-
-        /* fetch blacklisted user data */
-        const includeOptions: Prisma.UserInclude<DefaultArgs> = {};
-
-        switch (selection) {
-            case 'punishments': {
-                includeOptions.issuedPunishments = true;
-                includeOptions.receivedPunishments = true;
-                break;
-            }
-            case 'transactions': {
-                includeOptions.transactions = {
-                    select: {
-                        productCode: true,
-                        purchaseId: true,
-                        oneTimeTransferUsed: true,
-                        transactionType: true,
-                        updatedAt: true,
-                        createdAt: true,
-                    }
-                };
-                break;
-            }
-            case 'transfers': {
-                includeOptions.Transfers = true;
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-
-        const userData = await prisma.user.findFirst({
-            where: {
-                OR: [
-                    { discordId: user_id },
-                    { robloxId: user_id }
-                ]
-            },
-            include: includeOptions,
-        });
-        const blacklistUserData = await prisma.punishments.findFirst({
-            where: {
-                punishedUser: {
-                    id: userData?.id
-                }
-            }
-        })
-        const blacklistData = blacklistUserData;
-        /* send the user document */
-        await interaction.editReply({
-            embeds: [
-                ...(blacklistData ? [
-                    CustomEmbed.from({
-                        color: CustomEmbed.Color.Red,
-                        author: {
-                            icon_url: `${discord_client.user.displayAvatarURL({ forceStatic: false })}`,
-                            name: 'Inertia Lighting | User Blacklist System',
-                        },
-                        description: [
-                            '```',
-                            'User is blacklisted from using products!',
-                            '```',
-                            '```json',
-                            `${Discord.cleanCodeBlockContent(JSON.stringify(blacklistData, null, 2))}`,
-                            '```',
-                        ].join('\n'),
-                    }),
-                ] : []),
-                CustomEmbed.from({
-                    author: {
-                        icon_url: `${discord_client.user.displayAvatarURL({ forceStatic: false })}`,
-                        name: 'Inertia Lighting | User Lookup System',
-                    },
-                    description: [
-                        '```json',
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        `${Discord.cleanCodeBlockContent(JSON.stringify(userData ? (({ id, ...rest }) => rest)(userData) : 'user not found in database', null, 2))}`,
-                        '```',
-                    ].join('\n'),
-                }),
+          {
+            name: 'user',
+            type: Discord.ApplicationCommandOptionType.User,
+            description: 'The discord user to lookup.',
+            required: true,
+          },
+          {
+            name: 'properties',
+            type: Discord.ApplicationCommandOptionType.String,
+            description: 'Properties of the user to look up',
+            choices: [
+              { name: 'General', value: 'general' },
+              { name: 'Transactions', value: 'transactions' },
+              { name: 'Transfers', value: 'transfers' },
+              { name: 'Punishments', value: 'punishments' }
             ],
-        }).catch(console.warn);
-    },
+            required: false,
+          },
+          {
+            name: 'ephemeral',
+            type: Discord.ApplicationCommandOptionType.Boolean,
+            description: 'Whether or not to respond with an ephemeral message.',
+            required: false,
+          },
+        ],
+      }, {
+        name: 'roblox',
+        type: Discord.ApplicationCommandOptionType.Subcommand,
+        description: 'Uses roblox ids to lookup a user.',
+        options: [
+          {
+            name: 'user',
+            type: Discord.ApplicationCommandOptionType.String,
+            description: 'The roblox user id to lookup.',
+            required: true,
+          },
+          {
+            name: 'properties',
+            type: Discord.ApplicationCommandOptionType.String,
+            description: 'Properties of the user to look up',
+            choices: [
+              { name: 'General', value: 'general' },
+              { name: 'Transactions', value: 'transactions' },
+              { name: 'Transfers', value: 'transfers' },
+              { name: 'Punishments', value: 'punishments' }
+            ],
+            required: false,
+          },
+          {
+            name: 'ephemeral',
+            type: Discord.ApplicationCommandOptionType.Boolean,
+            description: 'Whether or not to respond with an ephemeral message.',
+            required: false,
+          },
+        ],
+      },
+
+    ],
+  },
+  metadata: {
+    required_run_context: CustomInteractionRunContext.Guild,
+    required_access_level: CustomInteractionAccessLevel.Staff,
+  },
+  handler: async (discord_client, interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.inCachedGuild()) return;
+    if (!interaction.channel) return;
+
+    const subcommand_name = interaction.options.getSubcommand(true);
+    const selection = interaction.options.getString('properties', false) as 'general' | 'transactions' | 'transfers' | 'punishments'
+    const respond_as_ephemeral: boolean = interaction.options.getBoolean('ephemeral', false) ?? false; // default to false
+
+    await interaction.deferReply({ flags: respond_as_ephemeral ? ['Ephemeral'] : [] });
+
+    let user_id: string;
+    switch (subcommand_name) {
+      case 'discord': {
+        user_id = interaction.options.getUser('user', true).id;
+
+        break;
+      }
+
+      case 'roblox': {
+        user_id = interaction.options.getString('user', true);
+
+        break;
+      }
+
+      default: {
+        throw new Error(`Unknown subcommand name: ${subcommand_name}`);
+      }
+    }
+
+    const user_id_regex = /^\d+$/;
+    if (!user_id_regex.test(user_id)) {
+      await interaction.editReply({
+        content: 'You supplied an invalid user id.',
+      });
+
+      return;
+    }
+
+    /* fetch blacklisted user data */
+    const includeOptions: Prisma.UserInclude<DefaultArgs> = {};
+
+    switch (selection) {
+      case 'punishments': {
+        includeOptions.issuedPunishments = true;
+        includeOptions.receivedPunishments = true;
+        break;
+      }
+      case 'transactions': {
+        includeOptions.transactions = {
+          select: {
+            productCode: true,
+            purchaseId: true,
+            oneTimeTransferUsed: true,
+            transactionType: true,
+            updatedAt: true,
+            createdAt: true,
+          }
+        };
+        includeOptions.receivedPunishments = true
+        break;
+      }
+      case 'transfers': {
+        includeOptions.Transfers = true;
+        includeOptions.receivedPunishments = true
+        break;
+      }
+      default: {
+        includeOptions.receivedPunishments = true
+        break;
+      }
+    }
+
+    const userData = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { discordId: user_id },
+          { robloxId: user_id }
+        ]
+      },
+      include: includeOptions,
+    });
+    const blacklistData = userData?.receivedPunishments.some((punishment) =>  punishment.punishmentType === 'blacklist');
+    /* send the user document */
+    await interaction.editReply({
+      embeds: [
+        ...(blacklistData ? [
+          CustomEmbed.from({
+            color: CustomEmbed.Color.Red,
+            author: {
+              icon_url: `${discord_client.user.displayAvatarURL({ forceStatic: false })}`,
+              name: 'Inertia Lighting | User Blacklist System',
+            },
+            description: [
+              '```',
+              'User is blacklisted from using products!',
+              '```',
+              '```json',
+              `${Discord.cleanCodeBlockContent(JSON.stringify(blacklistData, null, 2))}`,
+              '```',
+            ].join('\n'),
+          }),
+        ] : []),
+        CustomEmbed.from({
+          author: {
+            icon_url: `${discord_client.user.displayAvatarURL({ forceStatic: false })}`,
+            name: 'Inertia Lighting | User Lookup System',
+          },
+          description: [
+            '```json',
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            `${Discord.cleanCodeBlockContent(JSON.stringify(userData ? (({ id, ...rest }) => rest)(userData) : 'user not found in database', null, 2))}`,
+            '```',
+          ].join('\n'),
+        }),
+      ],
+    }).catch(console.warn);
+  },
 });
