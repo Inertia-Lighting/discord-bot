8fc468208e7fac0d70943a4b9b2235effc03f2af
Walter Robinson <chessebuilderman@gmail.com>
2025-10-07 18:23:34 -0700

    Remove legacy code

--- update.bat
+++
@@ -1,8 +0,0 @@
-if [[ `git status --porcelain` ]]; then
-  call git pull
-  call docker build --tag inertia-dicord-bot . 
-  call docker-compose doown 
-  call docker-compose up -d
-else
-  # No changes
-fi
