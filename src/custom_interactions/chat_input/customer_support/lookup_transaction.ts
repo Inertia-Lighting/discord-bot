2a602db955a6128774884c5944ee38096d4271e7
Cheese <41169564+chessebuilderman@users.noreply.github.com>
2026-02-27 17:53:06 -0800

    Create command (#581)

---
+++ src/custom_interactions/chat_input/customer_support/lookup_transaction.ts
@@ -0,0 +1,87 @@
+// ------------------------------------------------------------//
+//    Copyright (c) Inertia Lighting, Some Rights Reserved    //
+// ------------------------------------------------------------//
+
+import * as Discord from 'discord.js';
+
+import { CustomInteraction, CustomInteractionAccessLevel, CustomInteractionRunContext } from '@/common/managers/custom_interactions_manager.js'
+import { CustomEmbed } from '@/common/message.js'
+import prisma from '@/lib/prisma_client.js';
+
+// ------------------------------------------------------------//
+
+const bot_customer_service_role_id = `${process.env.BOT_CUSTOMER_SERVICE_ROLE_ID ?? ''}`;
+if (bot_customer_service_role_id.length < 1) throw new Error('Environment variable: BOT_CUSTOMER_SERVICE_ROLE_ID; is not set correctly.');
+
+// ------------------------------------------------------------//
+
+export default new CustomInteraction({
+    identifier: 'begin_recovery_process',
+    type: Discord.InteractionType.ApplicationCommand,
+    data: {
+        type: Discord.ApplicationCommandType.ChatInput,
+        description: 'Begin the process for account recovery.',
+        options: [
+            {
+                name: 'transaction_id',
+                type: Discord.ApplicationCommandOptionType.String,
+                description: 'The ID  of the transaction you would like to lookup.',
+                required: true,
+            },
+        ],
+    },
+    metadata: {
+        required_run_context: CustomInteractionRunContext.Guild,
+        required_access_level: CustomInteractionAccessLevel.CustomerService,
+    },
+    handler: async (discord_client, interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+        if (!interaction.inCachedGuild()) return;
+        if (!interaction.channel) return;
+
+        await interaction.deferReply();
+        const staff_member = interaction.member;
+        const staff_member_is_permitted = staff_member.roles.cache.has(bot_customer_service_role_id);
+        if (!staff_member_is_permitted) {
+            await interaction.editReply({
+                embeds: [
+                    CustomEmbed.from({
+                        color: CustomEmbed.Color.Violet,
+                        title: 'Inertia Lighting | Identity Manager',
+                        description: 'You aren\'t allowed to use this command!',
+                    }),
+                ],
+            });
+
+            return;
+        }
+        const transaction_id = interaction.options.getString('transaction_id', true);
+
+        const lookup = await prisma.transactions.findFirst({
+            where: {
+                purchaseId: transaction_id ?? ''
+            },
+            include: {
+                Transfers: true,
+                User: true,
+            }
+        }).catch(console.warn);
+
+        interaction.editReply({
+            embeds: [
+                CustomEmbed.from({
+                    author: {
+                        icon_url: `${discord_client.user.displayAvatarURL({ forceStatic: false })}`,
+                        name: 'Inertia Lighting | Transaction Lookup System',
+                    },
+                    description: [
+                        '```json',
+                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                        `${Discord.cleanCodeBlockContent(JSON.stringify(lookup ? (({ id, ...rest }) => rest)(lookup) : 'user not found in database', null, 2))}`,
+                        '```',
+                    ].join('\n'),
+                })
+            ]
+        })
+    },
+});
