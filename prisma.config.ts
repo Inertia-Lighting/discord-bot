c1727b7054dd446f554c98143032acdb53acdf5f
Cheese <41169564+chessebuilderman@users.noreply.github.com>
2026-03-03 01:23:11 -0800

    [FIX] Update schema (#583)
    * Add updated schema
    
    * Add submodule schema
    
    * Add schema
    
    * Delete old Prisma schema
    
    * Fix config
    
    * Add new version of schema
    
    * Update schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,9 @@ import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
+    schema: 'schema/prisma',
     migrations: {
-        path: 'prisma/migrations'
+        path: 'schema/prisma/migrations'
     },
     datasource: {
         url: env('DATABASE_URL')
