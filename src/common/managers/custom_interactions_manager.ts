682868b2c6bbf7a96baaa904fd3d0c6585a0a292
Walter Robinson <me@walterr.cc>
2026-05-09 23:52:08 -0700

    add logging

--- src/common/managers/custom_interactions_manager.ts
+++ src/common/managers/custom_interactions_manager.ts
@@ -265,6 +265,9 @@ export class CustomInteractionsManager {
 
     // eslint-disable-next-line complexity
     public static async handleInteractionFromDiscord(client: Discord.Client<true>, interaction: Discord.Interaction): Promise<void> {
+
+        console.log(interaction)
+
         let interaction_name: string;
 
         switch (interaction.type) {
